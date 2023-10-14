575a304a7c12f16f6d9a3fae71b3b2d279025274
dmxk062 <mxk06@protonmail.com>
2023-10-14 23:11:07 +0200

    added wallpapers

---
+++ .config/background/wallpaper.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+
+function set(){
+    if ! swww query
+    then
+        swww init
+        sleep 1
+    fi
+    swww img -t center "$HOME/.config/background/wall"
+}
+function set_as_wall(){
+    echo "new wall: $1"
+    unlink "$HOME/.config/background/wall"
+    ln -s "$1" "$HOME/.config/background/wall"
+}
+
+function set_as_lock(){
+    echo "new lock: $1"
+    unlink "$HOME/.config/background/lock"
+    ln -s "$1" "$HOME/.config/background/lock"
+}
+case $1 in
+    lock)
+        set_as_lock "$2"
+        ;;
+    wall)
+        set_as_wall "$2"
+        set
+        ;;
+    set)
+        set
+        ;;
+esac
