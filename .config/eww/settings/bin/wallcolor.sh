f544a9ad5ffea992c2ab573d98b85ec889453e38
dmxk062 <mxk06@protonmail.com>
2023-10-18 21:11:42 +0200

    round some numbers so they dont get too long

--- .config/eww/settings/bin/wallcolor.sh
+++ .config/eww/settings/bin/wallcolor.sh
@@ -4,6 +4,4 @@ answer="$(zenity --color-selection --title="Select a Background Color"|sed -e 's
 IFS=',' read -r r g b <<< $answer
 hex="$(printf "%02X%02X%02X" "$r" "$g" "$b")"
 swww clear $hex
-$HOME/.config/eww/bin/notify.sh "Set Background Color" "Hex: #$hex
-RGB: $r $g $b"
 
