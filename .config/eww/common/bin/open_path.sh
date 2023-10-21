0bf82b705ee2755b5d7eb8fc9d75ca87bc0c32d7
dmxk062 <mxk06@protonmail.com>
2023-10-21 15:39:19 +0200

    redid storage widget

---
+++ .config/eww/common/bin/open_path.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+eww -c $XDG_CONFIG_HOME/eww/settings/ close settings
+if [ -d "$1" ]
+then
+    nemo $1 --name=popup
+else
+    nemo "$(dirname $1)" --name=popup
+fi
+eww -c $XDG_CONFIG_HOME/eww/settings/ open settings --screen $(hyprctl -j monitors|jq '.[]|select(.focused).id')
