a2965800516a06330ba8e73587633a7ce19cbf2e
dmxk062 <mxk06@protonmail.com>
2023-10-27 19:07:05 +0200

    switch color for gtklock as well

--- .config/eww/settings/bin/colorscheme.sh
+++ .config/eww/settings/bin/colorscheme.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
-nohup $XDG_CONFIG_HOME/eww/settings/bin/color_scheme.sh > /dev/null &
+colorfile="$XDG_CONFIG_HOME/eww/style/color.scss"
+
+if grep -q 'dark' "$colorfile"
+then
+    sed -i 's/dark/light/' $XDG_CONFIG_HOME/eww/style/color.scss
+    color="light"
+else
+    sed -i 's/light/dark/' $XDG_CONFIG_HOME/eww/style/color.scss
+    color="dark"
+fi
+for EWW in popups settings top-bar
+do
+    eww -c $XDG_CONFIG_HOME/eww/$EWW reload
+done
+$HOME/.local/bin/eww_settings.sh
+sleep 0.1
+eww -c $XDG_CONFIG_HOME/eww/settings update colorscheme="$color"
+$XDG_CONFIG_HOME/eww/settings/bin/audio_state.sh
+$XDG_CONFIG_HOME/eww/settings/bin/sinks_sources.sh upd sinks & disown
+$XDG_CONFIG_HOME/eww/settings/bin/sinks_sources.sh upd sources & disown
+sassc $XDG_CONFIG_HOME/gtklock/style.scss > $XDG_CONFIG_HOME/gtklock/style.css
