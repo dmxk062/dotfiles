9d625f74e047d8b28bf575bbcc8b70ddb6870f6a
dmxk062 <mxk06@protonmail.com>
2023-11-05 13:11:55 +0100

    properly order windows in window list

--- .config/eww/top-bar/bin/open_window_list.sh
+++ .config/eww/top-bar/bin/open_window_list.sh
@@ -1,9 +1,47 @@
 #!/bin/sh
 
 eww="eww -c $XDG_CONFIG_HOME/eww/top-bar"
+workspace_order='{
+"web":1,
+"web2":2,
+"web3":3,
+"web4":4,
+"main":10,
+"1":11,
+"2":12,
+"3":13,
+"4":14,
+"5":15,
+"6":16,
+"7":17,
+"8":18,
+"9":19,
+"10":20,
+"11":21,
+"12":22,
+"13":23,
+"14":24,
+"15":25,
+"16":26,
+"17":27,
+"18":28,
+"19":29,
+"20":30,
+"21":31,
+"22":32,
+"23":33,
+"24":34,
+"games":300,
+"vm":350,
+"external":370,
+"special:1":400,
+"special:2":401,
+"special:3":402,
+"special:4":403
+}'
 
 update(){
-    hyprctl -j clients|jq 'map({
+    hyprctl -j clients|jq --argjson order "$workspace_order" 'map({
 title:.title,
 floating:.floating,
 class:.class,
@@ -12,8 +50,9 @@ legacy:.xwayland,
 workspace:.workspace,
 size:.size,
 position:.at,
-address:.address
-})|sort_by(.workspace.id)'
+address:.address,
+pos:$order[.workspace.name],
+})|sort_by(.pos)'
 }
 
 case $1 in
