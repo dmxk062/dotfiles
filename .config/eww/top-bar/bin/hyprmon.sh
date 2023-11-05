491ccd0de12c3dd60c06a3ba14dc598556243e8f
dmxk062 <mxk06@protonmail.com>
2023-11-05 13:05:44 +0100

    reduce the shitty icon system from hyprland based parts(window and workspaces)

--- .config/eww/top-bar/bin/hyprmon.sh
+++ .config/eww/top-bar/bin/hyprmon.sh
@@ -4,119 +4,6 @@ function update(){
     eww -c "$HOME/.config/eww/top-bar/" update "$@"
 }
 
-
-#
-# Windows
-#
-
-
-# window_icons='{
-# "kitty":"󰄛",
-# "firefox":"",
-# "nemo":"",
-# "zenity":"󰙵",
-# "steam":"󰓓",
-# "gamescope":"󰊴",
-# "org.prismlauncher.PrismLauncher":"󰍳",
-# "Tor Browser":"󰖟",
-# "mpv":"󱜅",
-# "gnome-boxes":"",
-# "gnome-disks":"󰋊",
-# "org.gnome.clocks":"󰥔",
-# "eog":"󰋩",
-# "libreoffice-startcenter":"󰈙",
-# "libreoffice-writer":"󰼭",
-# "libreoffice-calc":"󱖦",
-# "org.pwmt.zathura":"",
-# "thunderbird":"󰺻",
-# "Zotero":"󱫆",
-# "com.transmissionbt.transmission_39_364746":"",
-# "vinagre":"󰢹",
-# "Timeshift-gtk":"󰁯",
-# "valent":"",
-# "pavucontrol":"",
-# "virt-manager":"",
-# "polkit-gnome-authentication-agent-1":"󰟵",
-# "io.elementary.desktop.agent-polkit":"󰟵",
-# "com.obsproject.Studio":"",
-# "hyprland-share-picker":"󰩨",
-# "Vial":"󰌌",
-# "cava":"",
-# "simple-scan":"󰚫",
-# "com.github.flxzt.rnote":"󱦹",
-# "blueman-manager":"",
-# "blueman-sendto":"",
-# "nm-connection-editor":"󰖟",
-# "yuzu":"󰟡",
-# "soffice":"󰼭",
-# "seahorse":"󰌆",
-# "com.github.neithern.g4music":"󰁧",
-# "discord":"󰙯",
-# "fractal":"󰍩",
-# "io.github.celluloid_player.Celluloid":"󱜏"
-# }'
-
-function get_active_window_info(){
-    win="$(hyprctl activewindow -j)"
-    echo "$win" |jq -Mc '[.]|map({
-        id:.address,
-        pid:.pid,
-        class:.class,
-        title:.title,
-        wsname:.workspace.name,
-        shown:.mapped,
-        hidden:.hidden,
-        float:.floating,
-        pin:.pinned,
-        legacy:.xwayland,
-        fullscreen:.fullscreen,
-        mode:.fullscreenMode
-    })'
-}
-
-
-# 
-# Workspaces:
-#
-
-workspace_icons='{
-"main":"󰣇",
-"1":"1",
-"2":"2",
-"3":"3",
-"4":"4",
-"5":"5",
-"6":"6",
-"7":"7",
-"8":"8",
-"9":"9",
-"10":"10",
-"11":"11",
-"12":"12",
-"13":"13",
-"14":"14",
-"15":"15",
-"16":"16",
-"17":"17",
-"18":"18",
-"19":"19",
-"20":"20",
-"22":"22",
-"23":"23",
-"24":"24",
-"games":"󰊴",
-"web":"󰖟  1",
-"web2":"󰖟  2",
-"web3":"󰖟  3",
-"web4":"󰖟  4",
-"external":"󰍹  ext",
-"special:1":"󱓥  1",
-"special:2":"󱓥  2",
-"special:3":"󱜏",
-"special:4":"󰥔",
-"vm":"󰒋"
-}'
-
 workspace_order='{
 "web":1,
 "web2":2,
@@ -162,14 +49,13 @@ function get_active_workspace_id(){
 
 function list_workspaces(){
         active=$(get_active_workspace_id)
-        hyprctl workspaces -j|jq --argjson order "$workspace_order" --argjson icons "$workspace_icons" --argjson activeid "$active" 'map({
+        hyprctl workspaces -j|jq --argjson order "$workspace_order" --argjson activeid "$active" 'map({
             id:.id,
             name:.name,
             display:.monitor,
             count:.windows,
             title:.lastwindowtitle,
             fullscreen:.hasfullscreen,
-            icon:$icons[.name],
             pos:$order[.name],
             special:(if .name | test("special:.*") then true else false end),
             active:(if .id == $activeid then true else false end)
@@ -177,7 +63,7 @@ function list_workspaces(){
 
 }
 function refresh(){
-    update window="$(get_active_window_info)"
+    update window="$(hyprctl activewindow -j)"
     update workspaces="$(list_workspaces)"
 }
 
