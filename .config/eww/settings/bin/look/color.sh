4349f1a4bce4feb4fd2fca198b4263c5bc4b857c
dmxk062 <mxk06@protonmail.com>
2023-12-08 19:11:22 +0100

    add theme switching support for swaylock

--- .config/eww/settings/bin/look/color.sh
+++ .config/eww/settings/bin/look/color.sh
@@ -19,12 +19,16 @@ then
     sed -i 's/dark/light/' $XDG_CONFIG_HOME/rofi/style/color.rasi
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
     sed -i 's/dark.conf/light.conf/' $XDG_CONFIG_HOME/hypr/theme/colors.conf
+    unlink $XDG_CONFIG_HOME/swaylock/config
+    ln -s $XDG_CONFIG_HOME/swaylock/config.light $XDG_CONFIG_HOME/swaylock/config
     color="light"
 else
     sed -i 's/light.scss/dark.scss/' $XDG_CONFIG_HOME/eww/style/color.scss
     sed -i 's/light/dark/' $XDG_CONFIG_HOME/rofi/style/color.rasi
     sed -i 's/light.conf/dark.conf/' $XDG_CONFIG_HOME/hypr/theme/colors.conf
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+    unlink $XDG_CONFIG_HOME/swaylock/config
+    ln -s $XDG_CONFIG_HOME/swaylock/config.dark $XDG_CONFIG_HOME/swaylock/config
     color="dark"
 fi
 for EWW in settings shell
@@ -38,4 +42,4 @@ eww -c $XDG_CONFIG_HOME/eww/shell open desktop_area --screen 0
 $XDG_CONFIG_HOME/eww/settings/bin/audio_state.sh
 $XDG_CONFIG_HOME/eww/settings/bin/sinks_sources.sh upd sinks & disown
 $XDG_CONFIG_HOME/eww/settings/bin/sinks_sources.sh upd sources & disown
-sassc $XDG_CONFIG_HOME/gtklock/style.scss > $XDG_CONFIG_HOME/gtklock/style.css
+# sassc $XDG_CONFIG_HOME/gtklock/style.scss > $XDG_CONFIG_HOME/gtklock/style.css
