fe0fa83d3339255f7ea51c13e836e18d99918d3c
dmxk062 <mxk06@protonmail.com>
2023-12-10 16:23:33 +0100

    make dock animations better eww

--- .config/eww/shell/dock/bin/open_dock.sh
+++ .config/eww/shell/dock/bin/open_dock.sh
@@ -3,12 +3,22 @@
 
 eww="eww -c $HOME/.config/eww/shell"
 
-$eww update dock_reveal=false
-if ! $eww close dock_window 
-then
+# $eww update dock_reveal=false
+# sleep 0.2
+# if ! $eww close dock_window 
+# then
+#     $eww open dock_window
+#     $eww update dock_reveal=true
+#     $eww update apps="$(< $XDG_CONFIG_HOME/eww/shell/dock/apps.json)"
+# fi
+
+if [ $($eww get dock_reveal) == "true" ]; then
+    $eww update dock_reveal=false
+    sleep 0.2
+    $eww close dock_window
+else
     $eww open dock_window
     $eww update dock_reveal=true
     $eww update apps="$(< $XDG_CONFIG_HOME/eww/shell/dock/apps.json)"
 fi
-
 
