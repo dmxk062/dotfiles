c541c4154b8dfd4284b0311e54aebddf581833eb
dmxk062 <mxk06@protonmail.com>
2023-12-10 16:18:21 +0100

    stupid workaround for buggy eww variable behavior

--- .config/eww/shell/dock/bin/open_dock.sh
+++ .config/eww/shell/dock/bin/open_dock.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+
 eww="eww -c $HOME/.config/eww/shell"
 
+$eww update dock_reveal=false
 if ! $eww close dock_window 
 then
-    $eww update dock_reveal=true
     $eww open dock_window
+    $eww update dock_reveal=true
     $eww update apps="$(< $XDG_CONFIG_HOME/eww/shell/dock/apps.json)"
-else
-    $eww update dock_reveal=false
 fi
 
 
