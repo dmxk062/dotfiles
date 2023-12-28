4652664c75a4b8e48184c2ed173ae3bc7465ed8a
dmxk062 <mxk06@protonmail.com>
2023-12-28 20:10:37 +0100

    improve swayidle

--- .config/swayidle/idle.sh
+++ .config/swayidle/idle.sh
@@ -3,41 +3,22 @@ ICONDIR="/usr/share/icons/Tela/scalable"
 LOCK_CMD="swaylock --grace=6 --fade-in=5"
 LOCK_PRE_SUSPEND_CMD="swaylock"
 SLEEP_CMD="systemctl suspend"
+ICON="${ICONDIR}/apps/preferences-desktop-screensaver.svg"
 
 function notify(){
-    case $1 in
-        lock)
-            icon="${ICONDIR}/apps/preferences-desktop-screensaver.svg";;
-        sleep)
-            icon="${ICONDIR}/apps/system-suspend.svg";;
-    esac
     notify-send "Power Management" \
                 -a "swayidle" \
-                -i "$icon" \
+                -i "$ICON" \
                 "$2"
 }
 
 if [ "$1" == "notify" ]; then
-    shift
-    case $1 in
-        lock)
-            notify lock "Locking the session in 30 seconds"
-            ;;
-        suspend)
-            notify sleep "Suspending the system in 30 seconds"
-            ;;
-    esac
+    notify lock "Locking the session in 30 seconds"
 else
-    case $1 in
-        lock)
-            $LOCK_CMD
-            ;;
-        sleep)
-            $LOCK_PRE_SUSPEND_CMD
-            $SLEEP_CMD
-            ;;
-        pre)
-            $LOCK_PRE_SUSPEND_CMD
-            ;;
-    esac
+    if pgrep swaylock; then
+        eval "$SLEEP_CMD"
+    else
+        eval "$LOCK_CMD"
+    fi
 fi
+
