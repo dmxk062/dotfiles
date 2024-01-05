7f65977f20d4a56143951f03b4ca7a953801b121
dmxk062 <mxk06@protonmail.com>
2024-01-05 19:43:32 +0100

    eww: make more stuff listening for better features(dynamic speed for network) and fewer started shells

--- .config/eww/shell/bin/battery.sh
+++ .config/eww/shell/bin/battery.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
 
 BATDEV="/sys/devices/LNXSYSTM:00/LNXSYBUS:00/PNP0A08:00/device:41/PNP0C09:00/PNP0C0A:00/power_supply/BAT0"
-max=$(< $BATDEV/energy_full)
-percentage=$(< $BATDEV/capacity)
-status=$(< $BATDEV/status)
-wattage=$(< $BATDEV/power_now)
-current=$(< $BATDEV/energy_now)
 
-if [[ $status == "Charging" ]]
-then 
-    charging=true
-else
-    charging=false
-fi
 
-if $charging
-then
-    remaining=$((max - current))
-else
-    remaining=$current
-fi
-printf '{"max":%s,"perc":%s,"charging":%s,"watts":%s,"current":%s,"remaining":%s}' "$max" "$percentage" "$charging" "$wattage" "$current" "$remaining"
+while true; do
+    max=$(< $BATDEV/energy_full)
+    percentage=$(< $BATDEV/capacity)
+    status=$(< $BATDEV/status)
+    wattage=$(< $BATDEV/power_now)
+    current=$(< $BATDEV/energy_now)
+
+    if [[ $status == "Charging" ]]
+    then 
+        charging=true
+    else
+        charging=false
+    fi
+
+    if $charging
+    then
+        remaining=$((max - current))
+    else
+        remaining=$current
+    fi
+    printf '{"max":%s,"perc":%s,"charging":%s,"watts":%s,"current":%s,"remaining":%s}' "$max" "$percentage" "$charging" "$wattage" "$current" "$remaining"
+    sleep 12
+done
 # printf '{"max":71690000,"perc":50,"charging":false,"watts":8097000,"current":54040000,"remaining":71690000}'
 
 
--- .config/eww/shell/bin/netspeed.sh
+++ .config/eww/shell/bin/netspeed.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
-read -r down up <<< "$(LC_ALL=C sar -n DEV 1 1|awk '$1 == "Average:"&& $2 == "enp6s0" {print $5" "$6}')"
+#!/bin/zsh
+UPMAX=400
+DOWNMAX=400
 function nice(){
     echo $((${1::-3}*1024))|numfmt --to=iec
 }
-upnice="$(nice $up)"
-downnice="$(nice $down)"
-printf '{"raw":{"up":%s,"down":%s},"nice":{"up":"%s","down":"%s"}}' "$up" "$down" "$upnice" "$downnice"
+while true; do
+    read -r down up <<< "$(LC_ALL=C sar -n DEV 1 1|awk '$1 == "Average:"&& $2 == "enp6s0" {print $5" "$6}')"
+    upnice="$(nice $up)"
+    downnice="$(nice $down)"
+    if (( down > DOWNMAX)); then
+        DOWNMAX=$down
+    fi
+    if (( up > UPMAX)); then
+        UPMAX=$up
+    fi
+    printf '{"max":{"up":%s, "down":%s}, "raw":{"up":%s,"down":%s},"nice":{"up":"%s","down":"%s"}}\n' \
+        "$UPMAX" "$DOWNMAX" "$up" "$down" "$upnice" "$downnice"
+    sleep 3
+done
+
+
