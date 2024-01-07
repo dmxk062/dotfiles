69ab4d21573bd741d5519b94be8ead941fe134dc
dmxk062 <mxk06@protonmail.com>
2024-01-07 19:17:09 +0100

    hypr/eww: better overview handling

---
+++ .config/hypr/plugins/overview.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+if hyprctl workspaces | grep -q "OVERVIEW"; then
+    hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset; keyword general:border_size 0" 
+else
+    hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap overview; keyword general:border_size 2" 
+fi
