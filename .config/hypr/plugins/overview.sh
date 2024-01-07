6ce2a8b2f80d063883af1d4b286f842ff43f9abe
dmxk062 <mxk06@protonmail.com>
2024-01-07 19:49:39 +0100

    hypr: fix unescapable overview

--- .config/hypr/plugins/overview.sh
+++ .config/hypr/plugins/overview.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
 
-if hyprctl workspaces | grep -q "OVERVIEW"; then
-    hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset; keyword general:border_size 0" 
-else
-    hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap overview; keyword general:border_size 2" 
-fi
+case $1 in
+    off)
+        hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset; keyword general:border_size 0" 
+        ;;
+    *)
+        if hyprctl workspaces | grep -q "OVERVIEW"; then
+            hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset; keyword general:border_size 0" 
+        else
+            hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap overview; keyword general:border_size 2" 
+        fi;;
+esac
+
