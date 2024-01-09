a45ec8273e9ad33d5ce08675eba603d81471d5c3
dmxk062 <mxk06@protonmail.com>
2024-01-09 20:52:12 +0100

    hypr: overview no longer activates for special ws

--- .config/hypr/plugins/overview.sh
+++ .config/hypr/plugins/overview.sh
@@ -13,7 +13,13 @@ case $1 in
         if hyprctl workspaces | grep -q "OVERVIEW"; then
             hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset; keyword general:border_size 0" 
         else
-            if [[ "$(hyprctl clients -j|jq --argjson active "$(hyprctl monitors -j|jq '.[]|select(.focused)|.id')" '[.[]|select(.monitor == $active)]|length')" -gt 0 ]]; then
+            # we dont want special workspaces 
+            if [[ "$(hyprctl clients -j|jq --argjson active "$(hyprctl monitors -j|jq '.[]|select(.focused)|.id')" '[.
+                []|select(.monitor == $active 
+                    and .mapped 
+                    and (.workspace.name | startswith("special:") | not ) 
+                )
+                ]|length')" -gt 0 ]]; then
                 hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap overview; keyword general:border_size 2" 
             fi
         fi;;
