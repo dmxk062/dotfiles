98aba9d40ebd3d371d97657189ff134d0cd205c2
dmxk062 <mxk06@protonmail.com>
2024-01-13 13:55:47 +0100

    eww: better app grid handling

--- .config/eww/shell/dock/bin/nwg_drawer.sh
+++ .config/eww/shell/dock/bin/nwg_drawer.sh
@@ -1,21 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$1" == "--initial" ]]; then
-    nwg-drawer -c 8 \
-               -fm nautilus \
-               -ovl \
-               -nofs \
-               -term "kitty" \
-               -is 64 \
-               -nocats -r & disown 
-    exit
-fi
-
-
+killall nwg-drawer
+sleep 0.1
 nwg-drawer -c 8 \
            -fm nautilus \
            -ovl \
            -nofs \
            -term "kitty" \
            -is 64 \
-           -nocats & disown 
+           -nocats -r & disown 
