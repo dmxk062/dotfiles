3c0e844a3e47644ff8a06ae348025c7ea47abcfa
dmxk062 <mxk06@protonmail.com>
2024-01-13 12:20:37 +0100

    add intial to nwg

--- .config/eww/shell/dock/bin/nwg_drawer.sh
+++ .config/eww/shell/dock/bin/nwg_drawer.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+if [[ "$1" == "--initial" ]]; then
+    nwg-drawer -c 8 \
+               -fm nautilus \
+               -ovl \
+               -nofs \
+               -term "kitty" \
+               -is 64 \
+               -nocats -r & disown 
+    exit
+fi
+
+
 nwg-drawer -c 8 \
            -fm nautilus \
            -ovl \
