de96aac171369e0cb361fef9440d012be303f10b
dmxk062 <mxk06@protonmail.com>
2024-01-29 23:01:56 +0100

    eww: prompt: search and maths modes

--- .config/eww/shell/prompt/open.sh
+++ .config/eww/shell/prompt/open.sh
@@ -5,7 +5,7 @@ mode="$1"
 if $eww close prompt_window; then
     hyprctl dispatch submap reset
 else
-    $eww open prompt_window
+    $eww open prompt_window --screen $(hyprctl monitors -j | jq '.[]|select(.focused)|.id')
     hyprctl dispatch submap prompt
     if [[ -n $mode ]]; then
         $eww update prompt_current="$mode"
--- .config/eww/shell/prompt/process.sh
+++ .config/eww/shell/prompt/process.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 LOGFILE="/tmp/eww/state/prompt/history"
 INDEXFILE="/tmp/eww/state/prompt/index"
+MAX_COUNT=512
+MAX_DEPTH=6
 update(){
     eww -c $XDG_CONFIG_HOME/eww/shell update $@
 }
@@ -40,7 +42,27 @@ if [[ "$FULL_COMMAND" == "" ]]; then
     exit
 fi
 
-update prompt_current='' prompt_is_loading='true'
+search_files(){
+    cd "$2"||exit
+    echo "["
+    l="$(fd "$1" -ad $MAX_DEPTH --type file --threads=16 -x echo '{{ "path":"{}", "name":"{/}", "parent":"{//}" }},'|head -n $MAX_COUNT)"
+    echo "${l%?}"
+    echo "]"
+
+}
+search_dirs(){
+    cd "$2"||exit
+    echo "["
+    l="$(fd "$1" -ad $MAX_DEPTH --type directory --threads=16 -x echo '{{ "path":"{}", "name":"{/}" }},' |head -n $MAX_COUNT)"
+    echo "${l%?}"
+    echo "]"
+
+}
+prefind_dir(){
+    fd -aL --max-results 2 --type directory -d 3 "$1"|head -n 1
+}
+
+update prompt_current='' prompt_is_loading='true' prompt_result_type='regular'
 
 case $MODE in
     run)
@@ -48,11 +70,65 @@ case $MODE in
         command_thread "$COMMAND" & disown
         close
         ;;
+    launch)
+        COMMAND="${FULL_COMMAND/":"/}"
+        gtk-launch "$COMMAND" & disown
+        close
+        ;;
+    math)
+        expr="${FULL_COMMAND/"%"/}"
+        result="$(\qalc $expr|sed -e ':a;N;$!ba;s/\n/\\n/g' -e 's/"/\\"/g')"
+        stripped="${result/ /}"
+        if grep -q "error" <<< "$stripped"; then
+            error=true
+            result_value=""
+        else
+            error=false
+            IFS='=' read -ra fields <<< "$stripped"
+            result_value="${fields[-1]}"
+        fi
+        old="$(eww -c $XDG_CONFIG_HOME/eww/shell get "prompt_math_results")"
+        new="$(printf '{"input":"%s", "output":"%s", "result":"%s", "error":%s}' "$expr" "$result" "$result_value" "$error")"
+        updated="$(echo "$old"|jq --argjson new "$new" '. |= [$new] + .')"
+        eww -c $XDG_CONFIG_HOME/eww//shell update prompt_mode='default' prompt_error='false' \
+            prompt_show_help='false'  prompt_has_result='true' prompt_result_type='math'
+        update_single prompt_math_results "$updated"
+        ;;
+    search_dir)
+        cd || exit
+        IFS='\' read -r _ pre actual <<< "${FULL_COMMAND}"
+        if [[ "$actual" != "" ]]; then
+            predir="$(prefind_dir "$pre")"
+            searchterm="$actual"
+        else
+            predir="$HOME"
+            searchterm="$pre"
+        fi
+        matches="$(search_dirs "$searchterm" "$predir")"
+        eww -c $XDG_CONFIG_HOME/eww//shell update prompt_mode='default' prompt_error='false' \
+            prompt_show_help='false'  prompt_has_result='true' prompt_result_type='search' prompt_search_item="dir" 
+        update_single prompt_search_result "$matches"
+        ;;
+    search_file)
+        cd || exit
+        IFS='/' read -r _ pre actual <<< "${FULL_COMMAND}"
+        if [[ "$actual" != "" ]]; then
+            predir="$(prefind_dir "$pre")"
+            searchterm="$actual"
+        else
+            predir="$HOME"
+            searchterm="$pre"
+        fi
+        matches="$(search_files "$searchterm" "$predir")"
+        eww -c $XDG_CONFIG_HOME/eww//shell update prompt_mode='default' prompt_error='false' \
+            prompt_show_help='false' prompt_has_result='true' prompt_result_type='search' prompt_search_item="file"
+        update_single prompt_search_result "$matches"
+        ;;
     default)
         case $FULL_COMMAND in
             clear|c|:c)
                 rm "$LOGFILE"
-                update prompt_mode='default' prompt_error='false' prompt_show_help='false' prompt_has_result='false'
+                update prompt_mode='default' prompt_error='false' prompt_show_help='false' prompt_has_result='false' prompt_search_result='[]' prompt_math_results='[]'
                 ;;
             help|h|:h)
                 update prompt_mode='default' prompt_error='false' prompt_show_help='true' prompt_has_result='false'
@@ -94,5 +170,5 @@ case $MODE in
 esac
 echo "$FULL_COMMAND" >> "$LOGFILE"
 len=$(wc -l < "$LOGFILE" )
-echo $len > "$INDEXFILE"
+echo $((len+1)) > "$INDEXFILE"
 update prompt_hist="{\"len\":$len,\"pos\":$len,\"pos_f\":\"new\"}" prompt_is_loading='false'
