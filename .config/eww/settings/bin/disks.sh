dbe75ca4ef53f4bd0afba6d9fd3f1afe86896753
dmxk062 <mxk06@protonmail.com>
2024-01-31 18:15:22 +0100

    eww: get rid of last few hardcoded paths

--- .config/eww/settings/bin/disks.sh
+++ .config/eww/settings/bin/disks.sh
@@ -2,12 +2,12 @@
 eww="eww -c $HOME/.config/eww/settings"
 #lsblk -J --output=LABEL,NAME,PATH,PARTUUID,UUID,TRAN,VENDOR,FSSIZE,FSUSE%,FSUSED,MODEL,VENDOR,MOUNTPOINTS,PARTTYPENAME,FSTYPE
 icons_g='{
-"sata":"scalable/devices/drive-harddisk-ieee1394.svg",
-"nvme":"scalable/devices/drive-harddisk-solidstate.svg",
-"usb":"scalable/devices/media-flash-memory-stick.svg",
-"default":"scalable/devices/drive-harddisk.svg",
-"mmc":"scalable/devices/media-flash.svg",
-"mem":"scalable/devices/media-memory.svg"
+"sata":"drive-harddisk-ieee1394",
+"nvme":"drive-harddisk-solidstate",
+"usb":"media-flash-memory-stick",
+"default":"drive-harddisk",
+"mmc":"media-flash",
+"mem":"media-memory"
 }'
 icons='{
 "sata":"󰋊",
