70d30eb7c00435c6797fe3e9f91549a1ee498749
dmxk062 <mxk06@protonmail.com>
2024-02-29 17:36:54 +0100

    zsh: improve session start script

--- .config/zsh/run_session.sh
+++ .config/zsh/run_session.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env zsh
 
 
-printf "Hi ${USER}!\n"
+print "Hi ${USER}!"
 
 if [[ "${TTY}" == "/dev/tty1" ]]
 then
-    if [[ -n $DISPLAY ]]
-    then
-        echo -en "There already is a display server running. Dropping you to a shell (${SHELL}).\n\n"
+    if [[ -n $DISPLAY ]] || [[ -n $WAYLAND_DISPLAY ]]; then
+        print "There already is a display server running. Dropping you to a shell (${SHELL}).\n"
         exec $SHELL
     else
         if command -v Hyprland
         then
-            echo -en "\033[32mHanding off control to Hyprland now\033[0m\n"
+            print -P "%F{green}Handing off control to Hyprland now%f"
             exec Hyprland
         else
-            echo -en "\033[31mHyprland is not installed\033[0m\nDropping you to a shell (${SHELL})\n\n"
+            print -P "%F{red}Hyprland is not installed%f\nDropping you to a shell (${SHELL})\n"
             exec $SHELL
         fi
     fi
 else 
-    echo -en "You logged in at \033[1m${TTY}\033[0m.\nTo get a graphical environment with Hyprland, please log in at \033[1m/dev/tty1 \033[0m(press \033[1m<C-M-F1>\033[0m to get there)\nDropping you to a shell (${SHELL}).\n\n"
+    print -P "You logged in at %B${TTY}%b\nTo get a graphical environment with Hyprland, please log in at %B/dev/tty1%b (press %B<C-M-F1>%b to get there)\nDropping you to a shell (${SHELL}).\n\n"
     exec $SHELL
 fi
