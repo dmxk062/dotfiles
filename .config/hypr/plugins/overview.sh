8205b094cd85df0a2530cec8a156cc1e7d8f5626
dmxk062 <mxk06@protonmail.com>
2024-03-19 19:07:41 +0100

    hypr: unbreak hycov(hacky)

--- .config/hypr/plugins/overview.sh
+++ .config/hypr/plugins/overview.sh
@@ -6,12 +6,16 @@ case $1 in
         hyprctl --batch "dispatch submap reset; dispatch hycov:leaveoverview; keyword general:border_size 0" 
         ;;
     enter)
+        # work around bug that idk how to fix
         hyprctl --batch "dispatch submap overview; keyword general:border_size 2"
         ;;
     *)
         if hyprctl workspaces | grep -q "OVERVIEW"; then
             hyprctl --batch "dispatch hycov:toggleoverview; dispatch submap reset" 
         else
+            for addr in $(hyprctl clients -j|jq -r '.[]|select(.fullscreen).address'); do
+                hyprctl --batch "dispatch focuswindow address:$addr; dispatch fullscreen; dispatch focuscurrentorlast"
+            done
             # we dont want special workspaces 
             if [[ "$(hyprctl clients -j|jq --argjson active "$(hyprctl monitors -j|jq '.[]|select(.focused)|.id')" '[.
                 []|select(.monitor == $active 
