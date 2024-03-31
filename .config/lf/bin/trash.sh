fd3643851a8dd3aed358a083fb46d48001e60d23
dmxk062 <mxk06@protonmail.com>
2024-03-31 10:51:23 +0200

    lf: start working on new pv script

--- .config/lf/bin/trash.sh
+++ .config/lf/bin/trash.sh
@@ -1,4 +1,40 @@
-#!/bin/bash -eu
-IFS=$'\n'
+#!/usr/bin/zsh
 
-function list
+function notify {
+    lf -remote "send $id echomsg $1"
+}
+
+function add {
+    gio trash "$@"  
+    if (($# == 1)) {
+        name=file
+    } else {
+        name=files
+    }
+    notify "󰩹 Moved $# ${name} to trash"
+}
+
+function restore {
+    if (($# == 1)) {
+        name=file
+    } else {
+        name=files
+    }
+    for file in "$@"; do
+        basename="${file:t}"
+        gio trash --restore trash:///"$basename"
+    done
+    sleep .1
+    notify "󰩹 Restored $# ${name} from trash"
+}
+
+case "$1" in
+    add)
+        shift
+        add "$@"
+        ;;
+    restore)
+        shift
+        restore "$@"
+        ;;
+esac
