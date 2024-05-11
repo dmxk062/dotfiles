00cae026bf4033df849ef80854811a1993ae7799
dmxk062 <mxk06@protonmail.com>
2024-05-11 14:08:44 +0200

    nvim: add lmap to utils for buffer local mappings

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -8,6 +8,13 @@ function utils.map(mode, keys, action, opts)
     vim.keymap.set(mode, keys, action, opts or {})
 end
 
+-- map locally for only one buffer
+function utils.lmap(bufnr, mode, keys, action, opts)
+    opts = opts or {}
+    opts.buffer = bufnr
+    vim.keymap.set(mode, keys, action, opts)
+end
+
 function utils.abbrev(mode, keys, string)
     vim.api.nvim_command(mode .. "abbrev" .. " " .. keys .. " " .. string)
 end
