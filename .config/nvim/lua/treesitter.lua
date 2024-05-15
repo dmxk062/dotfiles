a4684c8c5700312ca72032fdea632e969b1c3862
dmxk062 <mxk06@protonmail.com>
2024-05-15 20:36:31 +0200

    nvim: move headlines setup to markdown

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -65,21 +65,3 @@ require('nvim-treesitter.configs').setup {
   }
 }
 
-require("headlines").setup({
-    markdown = {
-        headline_highlights = {
-            "Headline1",
-            "Headline2",
-            "Headline3",
-            "Headline4",
-            "Headline5",
-            "Headline6",
-        },
-        bullets = {""},
-        dash_string = "-",
-        codeblock_highlight = "CodeBlock",
-        dash_highlight = "Dash",
-        quote_highlight = "Quote",
-    },
-})
-
