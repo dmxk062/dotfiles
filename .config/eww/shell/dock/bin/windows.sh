590837ad96287c4066b921e3f4fb1c15ba8eebed
dmxk062 <mxk06@protonmail.com>
2024-05-22 17:48:24 +0200

    eww: sort windows by position and workspace in dock

--- .config/eww/shell/dock/bin/windows.sh
+++ .config/eww/shell/dock/bin/windows.sh
@@ -4,7 +4,7 @@ workspace_order='{
 "web":1, "web2":2, "web3":3, "web4":4, "main":10, "1":11, "2":12, "3":13, "4":14, "5":15, "6":16, "7":17, "8":18, "9":19, "10":20, "11":21, "12":22, "13":23, "14":24, "15":25, "16":26, "17":27, "18":28, "19":29, "20":30, "21":31, "22":32, "23":33, "24":34, "games":300, "srv":350, "external":370, "mirror":371, "special:1":400, "special:2":401, "special:3":402, "special:4":403, "OVERVIEW":500 }'
 
 update(){
-    hyprctl -j clients|jq -cM --argjson order "$workspace_order" 'map(. + {pos:$order[.workspace.name],})|sort_by(.pos)'
+    hyprctl -j clients|jq -cM --argjson order "$workspace_order" 'map(. + {pos:$order[.workspace.name],})|sort_by(.at[0])|sort_by(.pos)'
 
 }
 if [[ "$1" == "listen" ]]; then
