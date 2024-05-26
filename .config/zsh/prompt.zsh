09554adcf3b2358758d8c5ff18d378f8c4c8691d
dmxk062 <mxk06@protonmail.com>
2024-05-26 15:17:43 +0200

    zsh: make prompt faster and improve formatting

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,30 +1,30 @@
 # we *need* EPOCHREALTIME for the prompt to be accurate
 zmodload zsh/datetime
 
-function __cmd_start_timestamp {
+# directly set the hooks instead of just adding to the hook, so ours runs first
+function preexec {
   _CMD_TIMER=$EPOCHREALTIME
 }
 
-add-zsh-hook preexec __cmd_start_timestamp
 
-function __cmd_end_timestamp {
+function precmd {
     if [[ $_CMD_TIMER ]]; then
         local elapsed_ms=$[ ($EPOCHREALTIME-$_CMD_TIMER) * 1000 ] elapsed
         if (( elapsed_ms > 60000 )) {
-            printf -v elapsed "%.3fm" $[ $elapsed_ms/60000.0 ]
+            # print everything over a minute as MM:SS
+            printf -v elapsed "%02.0f:%02.0f" $[ ($elapsed_ms/1000.0) / 60 ] $[ ($elapsed_ms/1000.0) % 60 ]
         } elif (( elapsed_ms > 100 )) {
             printf -v elapsed "%.2fs" $[ $elapsed_ms/1000.0 ]
-        } elif (( elapsed_ms > 0.1 )) {
+        } else  {
             printf -v elapsed "%.2fms" $elapsed_ms
-        } else {
-            printf -v elapsed "%.2fns" $[ $elapsed_ms ]
         }
         RPROMPT="%F%(?.%F{green}.%F{red})%S%(?.󰄬.󰅖 %?)%s %F{#4c566a}%f%K{#4c566a}󱎫 ${elapsed}%k%F{#4c566a}%f"
+        # dont change this every time we press enter
+        unset _CMD_TIMER
     fi
     # set the title
     print -Pn "\e]0;zsh: %~\a"
 }
-add-zsh-hook precmd __cmd_end_timestamp
 
 # PROMPT="%B%F{magenta}%S %n %s %B%F{cyan}%S󰉋 %(4~|%-1~/…/%24<..<%2~%<<|%4~)%s%f%b "
 PROMPT="%B%F{cyan}%S󰉋 %(4~|%-1~/…/%24<..<%2~%<<|%4~)%s%f%b "
