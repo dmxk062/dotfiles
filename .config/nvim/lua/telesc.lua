2a5b17996988470ed1808047094e43c0fba606de
dmxk062 <mxk06@protonmail.com>
2024-05-31 20:20:06 +0200

    nvim/telescope: add binds for up/down

--- .config/nvim/lua/telesc.lua
+++ .config/nvim/lua/telesc.lua
@@ -3,14 +3,18 @@ local utils = require("utils")
 local themes = require("telescope.themes")
 
 -- telescope.load_extension('fzf')
-local buffer_on_enter = { 
+local buffer_on_enter = {
     n = {
         ["<enter>"] = "select_drop",
-        ["<S-enter>"] = "select_tab_drop"
+        ["<S-enter>"] = "select_tab_drop",
+        ["<M-j>"] = "move_selection_next",
+        ["<M-k>"] = "move_selection_previous",
     },
     i = {
         ["<enter>"] = "select_drop",
-        ["<S-enter>"] = "select_tab_drop"
+        ["<S-enter>"] = "select_tab_drop",
+        ["<M-j>"] = "move_selection_next",
+        ["<M-k>"] = "move_selection_previous",
     },
 }
 
@@ -32,6 +36,8 @@ telescope.setup {
                 ["e"] = "file_edit",
                 ["s"] = "select_horizontal",
                 ["v"] = "select_vertical",
+                ["<M-j>"] = "move_selection_next",
+                ["<M-k>"] = "move_selection_previous",
             },
         },
         dynamic_preview_title = true,
@@ -40,13 +46,13 @@ telescope.setup {
         prompt_prefix = " ",
     },
     pickers = {
-        lsp_definitions = default_config{jump_type = "tab"},
+        lsp_definitions = default_config { jump_type = "tab" },
         diagnostics = default_config(),
         find_files = default_config(),
-        git_files = default_config{prompt_title = "Files in Git"},
+        git_files = default_config { prompt_title = "Files in Git" },
         live_grep = default_config(),
         grep_string = default_config(),
-        oldfiles = default_config{prompt_title = "History"},
+        oldfiles = default_config { prompt_title = "History" },
         registers = {
             theme = "cursor",
             mappings = {
@@ -78,7 +84,7 @@ telescope.setup {
             }
         },
         lsp_references = default_config(),
-        treesitter = default_config{prompt_title = "Symbols"},
+        treesitter = default_config { prompt_title = "Symbols" },
     },
     extensions = {
         ["ui-select"] = {
@@ -99,28 +105,29 @@ utils.map('n', 'gd', builtin.lsp_definitions)
 utils.map('n', 'gi', builtin.lsp_implementations)
 
 for _, map in ipairs({
-    {"D", builtin.diagnostics},
-    {"T", builtin.treesitter},
-    {"F", builtin.find_files},
-    {"gF", builtin.git_files},
-    {"h", builtin.oldfiles},
-    {"/", builtin.live_grep},
-    {"r", builtin.registers},
-    {"<space>", builtin.buffers},
-    {"#", builtin.grep_string}
-}) do 
+    { "D",       builtin.diagnostics },
+    { "T",       builtin.treesitter },
+    { "F",       builtin.find_files },
+    { "gF",      builtin.git_files },
+    { "h",       builtin.oldfiles },
+    { "/",       builtin.live_grep },
+    { "r",       builtin.registers },
+    { "<space>", builtin.buffers },
+    { "#",       builtin.grep_string }
+}) do
     utils.map("n", _prefix .. map[1], map[2])
 end
 
 function register_and_insert()
-    builtin.registers{
+    builtin.registers {
         mappings = {
             n = {
-                ["e"] = edit_register,
+                ["e"] = "edit_register",
             }
         }
     }
 end
+
 utils.map('i', '<C-R>', register_and_insert)
 
 telescope.load_extension("ui-select")
