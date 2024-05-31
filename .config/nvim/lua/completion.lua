af42a43829b10ecac6aaa9343c90e53020dac253
dmxk062 <mxk06@protonmail.com>
2024-05-31 10:11:21 +0200

    nvim: add completion scroll binds

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -51,51 +51,50 @@ window = {
     }
 },
 mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<M-j>"] = cmp.mapping.select_next_item(),
+    ["<M-k>"] = cmp.mapping.select_prev_item(),
 }),
 sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-    { name = 'buffer' },
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = "buffer" },
 })
 })
 
-cmp.setup.filetype('gitcommit', {
+cmp.setup.filetype("gitcommit", {
     sources = cmp.config.sources({
-        { name = 'git' }, 
+        { name = "git" }, 
     }, {
-        { name = 'buffer' },
+        { name = "buffer" },
     })
 })
 
 
-cmp.setup.filetype('markdown', {
+cmp.setup.filetype("markdown", {
     sources = cmp.config.sources({
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer' },
-        { name = 'nvim_lsp' },
-        { name = 'spell' } -- move spell to the bottom so it doesnt slow it down that much
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+        { name = "nvim_lsp" },
+        { name = "spell" } -- move spell to the bottom so it doesnt slow it down that much
     })
 })
-cmp.setup.cmdline({ '/', '?'}, {
+cmp.setup.cmdline({ "/", "?"}, {
     mapping = cmp.mapping.preset.cmdline(),
     sources = {
-        { name = 'buffer' }
+        { name = "buffer" }
     }
 })
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
     sources = cmp.config.sources({
-        { name = 'path' },
-        { name = 'cmdline' },
+        { name = "path" },
+        { name = "cmdline" },
     })
 })
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-local lspconfig = require("lspconfig")
