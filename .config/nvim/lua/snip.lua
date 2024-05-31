a9938a4bd54552e40217e1b928130e4cf18ce59e
dmxk062 <mxk06@protonmail.com>
2024-05-31 19:10:18 +0200

    nvim: clean up snippets

--- .config/nvim/lua/snip.lua
+++ .config/nvim/lua/snip.lua
@@ -1,7 +1,7 @@
 local ls=require("luasnip")
 local lsvs=require("luasnip.loaders.from_vscode")
 
-lsvs.lazy_load({ exclude = { "markdown" }})
+lsvs.lazy_load({ exclude = { "markdown", "all" }})
 lsvs.lazy_load({ paths = {vim.fn.stdpath "config" .. "/snippets/"} })
 
 vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump( 1) end, {silent = true})
