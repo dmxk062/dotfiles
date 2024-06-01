ebb3609dda5011d544f9b14dfd6ace73926b7042
dmxk062 <mxk06@protonmail.com>
2024-06-01 16:02:41 +0200

    eww: add ui for changing master mfact

--- .config/eww/shell/bin/tiling_layout.sh
+++ .config/eww/shell/bin/tiling_layout.sh
@@ -3,13 +3,13 @@
 KEY="general:layout"
 ICON="/usr/share/icons/Tela/scalable/apps/preferences-desktop-theme-windowdecorations.svg"
 case "$(hyprctl -j getoption "$KEY"|jq '.str' -r)" in
-    master)
-        keywd=dwindle
+    dwindle)
+        keywd=master
         ;;
     *)
-        keywd=master
+        keywd=dwindle
         ;;
 esac
 echo "$keywd"
 hyprctl keyword "$KEY" "$keywd"
-eww -c $XDG_CONFIG_HOME/eww/shell update tiling_layout="$keywd"
+eww -c "$XDG_CONFIG_HOME"/eww/shell update tiling_layout="$keywd"
