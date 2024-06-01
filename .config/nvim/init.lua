5a3340bef165932b76a58c017a6ee94f1aec3346
dmxk062 <mxk06@protonmail.com>
2024-06-01 17:19:45 +0200

    nvim: better titles

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,9 +27,54 @@ vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
 
 vim.o.title = true
-vim.o.titlestring = "nv: %F"
-
 vim.o.wrap = true
+
+vim.api.nvim_create_autocmd({"BufEnter", "BufReadPost", "BufNewFile"},{
+callback = function(args)
+    local function format_path(name, user)
+        local expanded = name:gsub("/tmp/workspaces_" .. user, "~tmp")
+            :gsub("/home/" .. user .. "/ws", "~ws")
+            :gsub("/home/" .. user .. "/.config", "~cfg")
+            :gsub("/home/" .. user, "~")
+        return expanded
+    end
+
+    local path = ""
+    local buf = vim.api.nvim_get_current_buf()
+    local bufname  = vim.api.nvim_buf_get_name(buf)
+    local filetype = vim.bo[buf]["filetype"]
+
+    local user = vim.env.USER
+
+    if filetype == "TelescopePrompt" then
+        path = ""
+    elseif filetype == "oil" then
+        if vim.startswith(bufname, "oil-ssh://") then
+            local remote_path = bufname:match("//.-(/.*)"):sub(2, -1) -- the path at the host
+            path = "ssh:" .. remote_path
+        else
+            path = format_path(bufname:sub(#"oil:///"), user)
+        end
+    elseif filetype == "vim-plug" then
+        path = "Plugins"
+    elseif filetype == "alpha" then
+        path = "NeoVIM"
+    elseif bufname == "" then
+        path = "[No Name]"
+    else
+        path = format_path(bufname, user)
+    end
+
+    vim.o.titlestring = "nv: " .. path
+end
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function ()
+        vim.o.titlestring = "nv: NeoVIM"
+    end
+})
+
 
 
 
