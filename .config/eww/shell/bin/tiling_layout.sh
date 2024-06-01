ac1aa2f1229c0ea2e72b3397abf9f473f1786923
dmxk062 <mxk06@protonmail.com>
2024-06-01 20:29:41 +0200

    eww/hypr: add ui to cycle master direction

--- .config/eww/shell/bin/tiling_layout.sh
+++ .config/eww/shell/bin/tiling_layout.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-KEY="general:layout"
-ICON="/usr/share/icons/Tela/scalable/apps/preferences-desktop-theme-windowdecorations.svg"
-case "$(hyprctl -j getoption "$KEY"|jq '.str' -r)" in
-    dwindle)
-        keywd=master
+case $1 in
+    orientation)
+        hyprctl dispatch layoutmsg orientationcycle left top
         ;;
     *)
-        keywd=dwindle
-        ;;
+        KEY="general:layout"
+        case "$(hyprctl -j getoption "$KEY"|jq '.str' -r)" in
+            dwindle)
+                keywd=master
+                ;;
+            master|*)
+                keywd=dwindle
+                ;;
+        esac
+        echo "$keywd"
+        hyprctl keyword "$KEY" "$keywd"
+        eww -c "$XDG_CONFIG_HOME"/eww/shell update tiling_layout="$keywd"
+    ;;
 esac
-echo "$keywd"
-hyprctl keyword "$KEY" "$keywd"
-eww -c "$XDG_CONFIG_HOME"/eww/shell update tiling_layout="$keywd"
