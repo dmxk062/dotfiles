140cbed53d3789ff6e470b3252a3789080419650
dmxk062 <mxk06@protonmail.com>
2024-06-08 18:12:06 +0200

    nvim: improve utils.abbrev by stopping to rely on nvim_command

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -21,14 +21,6 @@ function M.kitty_shell_in(uri, type, opts)
     vim.system(cmd, { detach = true })
 end
 
--- function M.kitty_new_dir(path, type)
---     vim.fn.jobstart(string.format("kitty @ launch --type=%s --cwd \"%s\" -- zsh -i", type, path))
--- end
---
--- function M.kitty_new_cmd(cmd, type)
---     vim.fn.jobstart("kitty @ launch --type=" .. type .. " -- " .. cmd)
--- end
-
 function M.map(mode, keys, action, opts)
     vim.keymap.set(mode, keys, action, opts or {})
 end
@@ -41,7 +33,7 @@ function M.lmap(bufnr, mode, keys, action, opts)
 end
 
 function M.abbrev(mode, keys, string)
-    vim.api.nvim_command(mode .. "abbrev" .. " " .. keys .. " " .. string)
+    vim.keymap.set(mode .. "a", keys, string)
 end
 
 return M
