14cdeceb97c819738a09bafdab632b776c459379
dmxk062 <mxk06@protonmail.com>
2024-06-08 16:38:42 +0200

    nvim: better layout for longer plugins

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,4 +1,4 @@
-return {
+M = {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
@@ -24,124 +24,131 @@ return {
                 vim.keymap.set({ "i", "s" }, "<C-S-Tab", function() ls.jump(-1) end, { silent = true })
             end
         },
-    },
-    config = function()
-        local lspkind = require("lspkind")
-        local cmp = require("cmp")
+    }
+}
+M.config = function()
+    local lspkind = require("lspkind")
+    local cmp = require("cmp")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            formatting = {
-                format = lspkind.cmp_format({
-                    mode = "symbol",
-                    symbol_map = {
-                        Text = "󰉿 txt",
-                        Method = "󰆧 method",
-                        Function = "󰊕 func",
-                        Constructor = " constructor",
-                        Field = "󰽐 field",
-                        Variable = "α var",
-                        Class = "󰠱 class",
-                        Interface = " type",
-                        Module = " module",
-                        Property = "󰜢 prop",
-                        Unit = "󰑭 unit",
-                        Value = "󰎠 val",
-                        Enum = " enum",
-                        Keyword = "󰌋 keywd",
-                        Snippet = " snip",
-                        Color = "󰏘 color",
-                        File = "󰈙 file",
-                        Reference = "󰈇 ref",
-                        Folder = " dir",
-                        EnumMember = " enum Member",
-                        Constant = "󰏿 const",
-                        Struct = "󰙅 struct",
-                        Event = " event",
-                        Operator = "󰆕 op",
-                        TypeParameter = " param",
-                    },
-                }),
-            },
-            window = {
-                completion = {
-                    border = 'rounded',
-                    winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None',
-                    scrollbar = false,
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)     -- For `luasnip` users.
+            end,
+        },
+        formatting = {
+            format = lspkind.cmp_format({
+                mode = "symbol",
+                symbol_map = {
+                    Text = "󰉿 txt",
+                    Method = "󰆧 method",
+                    Function = "󰊕 func",
+                    Constructor = " constructor",
+                    Field = "󰽐 field",
+                    Variable = "α var",
+                    Class = "󰠱 class",
+                    Interface = " type",
+                    Module = " module",
+                    Property = "󰜢 prop",
+                    Unit = "󰑭 unit",
+                    Value = "󰎠 val",
+                    Enum = " enum",
+                    Keyword = "󰌋 keywd",
+                    Snippet = " snip",
+                    Color = "󰏘 color",
+                    File = "󰈙 file",
+                    Reference = "󰈇 ref",
+                    Folder = " dir",
+                    EnumMember = " enum Member",
+                    Constant = "󰏿 const",
+                    Struct = "󰙅 struct",
+                    Event = " event",
+                    Operator = "󰆕 op",
+                    TypeParameter = " param",
                 },
-                documentation = {
-                    border = 'rounded',
-                    winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,CursorLine:CursorLine,Search:None',
-                    scrollbar = false,
+            }),
+        },
+        window = {
+            completion = {
+                border = "rounded",
+                winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+                scrollbar = false,
+            },
+            documentation = {
+                border = "rounded",
+                winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:CursorLine,Search:None",
+                scrollbar = false,
+            }
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<M-j>"] = cmp.mapping.select_next_item(),
+            ["<M-k>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "path" },
+            { name = "buffer" },
+        })
+    })
+
+    cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+            { name = "git" },
+        }, {
+            { name = "buffer" },
+        })
+    })
+
+
+    cmp.setup.filetype("markdown", {
+        sources = cmp.config.sources({
+            { name = "luasnip" },
+            { name = "path" },
+            { name = "buffer" },
+            { name = "nvim_lsp" },
+            { name = "spell" }     -- move spell to the bottom so it doesnt slow it down that much
+        })
+    })
+    cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = "buffer" }
+        }
+    })
+
+    cmp.setup.cmdline({ ":" }, {
+        sources = cmp.config.sources({
+            { name = "path" },
+            {
+                name = "cmdline",
+                option = {
+                    ignore_cmds = {}
                 }
             },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                ["<M-j>"] = cmp.mapping.select_next_item(),
-                ["<M-k>"] = cmp.mapping.select_prev_item(),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "path" },
-                { name = "buffer" },
-            })
         })
+    })
+    cmp.setup.filetype("oil", {
+        sources = cmp.config.sources({
+            {
+                name = "path",
+                option = {
+                    get_cwd = function()
+                        -- local pwd if ssh
+                        return require("oil").get_current_dir() or vim.fn.getcwd()
+                    end
+                }
+            },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "nvim_lsp" },
+        })
+    })
+end
 
-        cmp.setup.filetype("gitcommit", {
-            sources = cmp.config.sources({
-                { name = "git" },
-            }, {
-                { name = "buffer" },
-            })
-        })
-
-
-        cmp.setup.filetype("markdown", {
-            sources = cmp.config.sources({
-                { name = "luasnip" },
-                { name = "path" },
-                { name = "buffer" },
-                { name = "nvim_lsp" },
-                { name = "spell" } -- move spell to the bottom so it doesnt slow it down that much
-            })
-        })
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" }
-            }
-        })
-
-        cmp.setup.cmdline(":", {
-            sources = cmp.config.sources({
-                { name = "path" },
-                { name = "cmdline" },
-            })
-        })
-        cmp.setup.filetype("oil", {
-            sources = cmp.config.sources({
-                {
-                    name = "path",
-                    option = {
-                        get_cwd = function()
-                            -- local pwd if ssh
-                            return require("oil").get_current_dir() or vim.fn.getcwd()
-                        end
-                    }
-                },
-                { name = "luasnip" },
-                { name = "buffer" },
-                { name = "nvim_lsp" },
-            })
-        })
-    end
-}
+return M
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,239 +1,235 @@
-return {
+local M = {
     "stevearc/oil.nvim",
     dependencies = {
         "nvim-tree/nvim-web-devicons",
         "refractalize/oil-git-status.nvim"
     },
-    config = function()
-        local actions = require("oil.actions")
-        local api = require("oil")
-        local utils = require("utils")
-
-
-        local perms_hlgroups = {
-            ["-"] = "OilNoPerm",
-            ["r"] = "OilRead",
-            ["w"] = "OilWrite",
-            ["x"] = "OilExec",
-            ["t"] = "OilSticky",
-            ["s"] = "OilSetuid",
-        }
-
-
-        local function open_chmod()
-            actions.open_cmdline.callback()
-            vim.api.nvim_input("!chmod ")
-        end
-
-        local oil_columns = {
-            icon = {
-                "icon",
-                default_file = "󰈔",
-                directory = "",
-            },
-            permissions = {
-                "permissions",
-                highlight = function(str)
-                    local hls = {}
-                    for i = 1, #str do
-                        table.insert(hls, { perms_hlgroups[str:sub(i, i)], i - 1, i })
-                    end
-                    return hls
-                end,
-            },
-        }
-
-        api.setup({
-            default_file_explorer = true,
-
-
-            win_options = {
-                cursorlineopt = "line,number",
-                signcolumn    = "auto"
-            },
-            columns = {
-                oil_columns.icon,
-                oil_columns.permissions
-            },
-            constrain_cursor = "editable",
-            skip_confirm_for_simple_edits = true,
-
-            float = {
-                padding = 16,
-                max_width = 80,
-                max_height = 40,
-
-            },
-            preview = {
-                max_width = 0.6,
-                min_width = 0.4,
-                max_height = 0.8,
-                min_height = 0.6,
-
-            },
-            use_default_keymaps = true,
-            cleanup_delay_ms = 5000,
-            extra_scp_args = { "-O" }, -- use scp instead of sftp
-            experimental_watch_for_changes = true,
-
-            view_options = {
-                is_hidden_file = function(name, bufnr)
-                    return vim.startswith(name, ".") and not (name:sub(2, 2) == ".")
-                end,
-
-                is_always_hidden = function(name, bufnr)
-                    return name == "."
-                end,
-                natural_order = true,
-            },
-
-            keymaps = {
-                ["!"] = function()
-                    actions.open_cmdline.callback()
-                    vim.api.nvim_input("! ")
-                end,
-                ["<C-space>"] = actions.refresh,
-                ["<CR>"] = actions.select,
-                ["<S-CR>"] = actions.select_tab,
-                ["<C-CR>"] = actions.select_split,
-                ["<M-CR>"] = actions.select_vsplit,
-
-
-            },
-        })
-
-        -- automatically cd the whole nvim
-        vim.api.nvim_create_autocmd("User", {
-            pattern  = "OilEnter",
-            callback = function(bufnr)
-                -- change directory if not ssh
-                if api.get_current_dir() then
-                    actions.cd.callback()
-                end
-            end
-        })
-
-        -- all the actions for the keybinds
-        local function goto_dir(path)
-            api.open(vim.fn.expand(path))
-        end
-
-        local function open_cd()
-            vim.api.nvim_input("<ESC>:Oil ")
-        end
-
-        local function goto_git_ancestor()
-            api.open(require("lspconfig.util").find_git_ancestor(api.get_current_dir()))
-        end
-
-        local function toggle_git_shown()
-            if vim.wo.signcolumn == "no" then
-                vim.wo.signcolumn = "auto"
-            else
-                vim.wo.signcolumn = "no"
-            end
-        end
-
-        local function open_external()
-            local entry = api.get_cursor_entry()
-            local dir   = api.get_current_dir()
-            if not dir then
-                -- ssh, we hope that the application uses gio or whatever and can use sftp:// uris
-                -- if that doesnt work, try
-                -- `xdg-mime default <application>.desktop x-scheme-handler/sftp`
-                -- where application is an app that can talk sftp
-                -- org.gnome.Nautilus works for instance
-                -- TODO: write some hacky handler myself that uses gio to then open that
-                local bufname = vim.api.nvim_buf_get_name(0)
-                local addr = bufname:match("//(.-)/")
-                local remote_path = bufname:match("//.-(/.*)"):sub(2, -1)
-                local uri = "sftp://" .. addr .. remote_path .. api.get_cursor_entry().name
-                vim.fn.jobstart("xdg-open '" .. uri .. "'")
-            else
-                vim.fn.jobstart("xdg-open '" .. dir .. "/" .. entry.name .. "'")
-            end
-        end
-
-        local function open_dir_shell(type, where)
-            utils.kitty_shell_in(api.get_current_dir() or vim.api.nvim_buf_get_name(0), type, where)
-        end
-
-
-        -- TODO: use this for mappings instead
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = "oil",
-
-            callback = function()
-                local normal_mappings = {
-                    -- edit in <thing>
-                    { "es",  actions.select_split.callback },
-                    { "et",  actions.select_tab.callback },
-                    { "ev",  actions.select_vsplit.callback },
-
-                    { "eo",  open_external },
-
-                    -- edit permissions
-                    { "ep",  open_chmod },
-
-                    -- goto places
-                    { "g~",  function() goto_dir("~") end },
-
-                    { "gr",  function() api.open("/") end },
-                    { "g/",  function() api.open("/") end },
-
-                    { "gp",  actions.parent.callback },
-                    { "g..", actions.parent.callback },
-
-                    -- only applies to my machines
-                    { "gw",  function() goto_dir("~/ws") end },
-                    { "gt",  function() goto_dir("~/Tmp") end },
-
-                    { "gP",  goto_git_ancestor },
-
-                    -- toggle hidden
-                    { "gh",  actions.toggle_hidden.callback },
-                    { "gH",  actions.toggle_hidden.callback },
-                    { "gs",  toggle_git_shown },
-
-
-                    { "cd",  open_cd },
-
-                    { " sw", function() open_dir_shell("window") end },
-                    { " sv", function() open_dir_shell("window", "vsplit") end },
-                    { " ss", function() open_dir_shell("window", "hsplit") end },
-                    { " sW", function() open_dir_shell("os-window") end },
-                    { " so", function() open_dir_shell("overlay") end },
-                    { " st", function() open_dir_shell("tab") end },
-                }
-                for _, map in ipairs(normal_mappings) do
-                    utils.lmap(0, "n", map[1], map[2])
-                end
-            end,
-        })
-
-        local prefix = "<space>f"
-        utils.map("n", prefix .. "F", api.open_float)
-        utils.map("n", prefix .. "f", api.open)
-
-        utils.map("n", prefix .. "t", function()
-            vim.api.nvim_command("tabnew")
-            api.open()
-        end)
-        utils.map("n", prefix .. "s", function()
-            vim.api.nvim_command("split")
-            api.open()
-        end)
-        utils.map("n", prefix .. "v", function()
-            vim.api.nvim_command("vsplit")
-            api.open()
-        end)
-
-
-        local function oil_cmp_get_pwd()
-            -- return the local pwd if ssh
-            return api.get_current_dir() or vim.fn.getcwd()
-        end
-
-        require("oil-git-status").setup({})
-    end
 }
+M.config = function()
+    local actions = require("oil.actions")
+    local api = require("oil")
+    local utils = require("utils")
+
+
+    local perms_hlgroups = {
+        ["-"] = "OilNoPerm",
+        ["r"] = "OilRead",
+        ["w"] = "OilWrite",
+        ["x"] = "OilExec",
+        ["t"] = "OilSticky",
+        ["s"] = "OilSetuid",
+    }
+
+
+    local function open_chmod()
+        actions.open_cmdline.callback()
+        vim.api.nvim_input("!chmod ")
+    end
+
+    local oil_columns = {
+        icon = {
+            "icon",
+            default_file = "󰈔",
+            directory = "",
+        },
+        permissions = {
+            "permissions",
+            highlight = function(str)
+                local hls = {}
+                for i = 1, #str do
+                    table.insert(hls, { perms_hlgroups[str:sub(i, i)], i - 1, i })
+                end
+                return hls
+            end,
+        },
+    }
+
+    api.setup({
+        default_file_explorer = true,
+
+
+        win_options = {
+            cursorlineopt = "line,number",
+            signcolumn    = "auto"
+        },
+        columns = {
+            oil_columns.icon,
+            oil_columns.permissions
+        },
+        constrain_cursor = "editable",
+        skip_confirm_for_simple_edits = true,
+
+        float = {
+            padding = 16,
+            max_width = 80,
+            max_height = 40,
+
+        },
+        preview = {
+            max_width = 0.6,
+            min_width = 0.4,
+            max_height = 0.8,
+            min_height = 0.6,
+
+        },
+        use_default_keymaps = true,
+        cleanup_delay_ms = 5000,
+        extra_scp_args = { "-O" },     -- use scp instead of sftp
+        experimental_watch_for_changes = true,
+
+        view_options = {
+            is_hidden_file = function(name, bufnr)
+                return vim.startswith(name, ".") and not (name:sub(2, 2) == ".")
+            end,
+
+            is_always_hidden = function(name, bufnr)
+                return name == "."
+            end,
+            natural_order = true,
+        },
+
+        keymaps = {
+            ["!"] = function()
+                actions.open_cmdline.callback()
+                vim.api.nvim_input("! ")
+            end,
+            ["<C-space>"] = actions.refresh,
+            ["<CR>"] = actions.select,
+            ["<S-CR>"] = actions.select_tab,
+            ["<C-CR>"] = actions.select_split,
+            ["<M-CR>"] = actions.select_vsplit,
+
+
+        },
+    })
+
+    -- automatically cd the whole nvim
+    vim.api.nvim_create_autocmd("User", {
+        pattern  = "OilEnter",
+        callback = function(bufnr)
+            -- change directory if not ssh
+            if api.get_current_dir() then
+                actions.cd.callback()
+            end
+        end
+    })
+
+    -- all the actions for the keybinds
+    local function goto_dir(path)
+        api.open(vim.fn.expand(path))
+    end
+
+    local function open_cd()
+        vim.api.nvim_input("<ESC>:Oil ")
+    end
+
+    local function goto_git_ancestor()
+        api.open(require("lspconfig.util").find_git_ancestor(api.get_current_dir()))
+    end
+
+    local function toggle_git_shown()
+        if vim.wo.signcolumn == "no" then
+            vim.wo.signcolumn = "auto"
+        else
+            vim.wo.signcolumn = "no"
+        end
+    end
+
+    local function open_external()
+        local entry = api.get_cursor_entry()
+        local dir   = api.get_current_dir()
+        if not dir then
+            -- ssh, we hope that the application uses gio or whatever and can use sftp:// uris
+            -- if that doesnt work, try
+            -- `xdg-mime default <application>.desktop x-scheme-handler/sftp`
+            -- where application is an app that can talk sftp
+            -- org.gnome.Nautilus works for instance
+            -- TODO: write some hacky handler myself that uses gio to then open that
+            local bufname = vim.api.nvim_buf_get_name(0)
+            local addr = bufname:match("//(.-)/")
+            local remote_path = bufname:match("//.-(/.*)"):sub(2, -1)
+            local uri = "sftp://" .. addr .. remote_path .. api.get_cursor_entry().name
+            vim.fn.jobstart("xdg-open '" .. uri .. "'")
+        else
+            vim.fn.jobstart("xdg-open '" .. dir .. "/" .. entry.name .. "'")
+        end
+    end
+
+    local function open_dir_shell(type, where)
+        utils.kitty_shell_in(api.get_current_dir() or vim.api.nvim_buf_get_name(0), type, where)
+    end
+
+
+    -- TODO: use this for mappings instead
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "oil",
+
+        callback = function()
+            local normal_mappings = {
+                -- edit in <thing>
+                { "es",        actions.select_split.callback },
+                { "et",        actions.select_tab.callback },
+                { "ev",        actions.select_vsplit.callback },
+
+                { "eo",        open_external },
+
+                -- edit permissions
+                { "ep",        open_chmod },
+
+                -- goto places
+                { "g~",        function() goto_dir("~") end },
+
+                { "gr",        function() api.open("/") end },
+                { "g/",        function() api.open("/") end },
+
+                { "gp",        actions.parent.callback },
+                { "g..",       actions.parent.callback },
+
+                -- only applies to my machines
+                { "gw",        function() goto_dir("~/ws") end },
+                { "gt",        function() goto_dir("~/Tmp") end },
+
+                { "gP",        goto_git_ancestor },
+
+                -- toggle hidden
+                { "gh",        actions.toggle_hidden.callback },
+                { "gH",        actions.toggle_hidden.callback },
+                { "gs",        toggle_git_shown },
+
+
+                { "cd",        open_cd },
+
+                { "<space>sw", function() open_dir_shell("window") end },
+                { "<space>sv", function() open_dir_shell("window", "vsplit") end },
+                { "<space>ss", function() open_dir_shell("window", "hsplit") end },
+                { "<space>sW", function() open_dir_shell("os-window") end },
+                { "<space>so", function() open_dir_shell("overlay") end },
+                { "<space>st", function() open_dir_shell("tab") end },
+            }
+            for _, map in ipairs(normal_mappings) do
+                utils.lmap(0, "n", map[1], map[2])
+            end
+        end,
+    })
+
+    local prefix = "<space>f"
+    utils.map("n", prefix .. "F", api.open_float)
+    utils.map("n", prefix .. "f", api.open)
+
+    utils.map("n", prefix .. "t", function()
+        vim.api.nvim_command("tabnew")
+        api.open()
+    end)
+    utils.map("n", prefix .. "s", function()
+        vim.api.nvim_command("split")
+        api.open()
+    end)
+    utils.map("n", prefix .. "v", function()
+        vim.api.nvim_command("vsplit")
+        api.open()
+    end)
+
+    require("oil-git-status").setup({})
+end
+
+return M
