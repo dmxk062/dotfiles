a139961379bc7292007bc6ca31e6fe40b3030114
dmxk062 <mxk06@protonmail.com>
2024-06-29 10:13:11 +0200

    nvim: fully get rid of old theme

--- .config/nvim/lua/nord/colors/nord.lua
+++
@@ -1,7 +0,0 @@
-package.loaded['nord'] = nil
-package.loaded['nord.colors'] = nil
-package.loaded['nord.named_colors'] = nil
-package.loaded['nord.theme'] = nil
-package.loaded['nord.util'] = nil
-
-require('nord').set()
