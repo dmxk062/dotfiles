19cdc25c2ca5e6a027dc49332a8341d5dc231b4d
dmxk062 <mxk06@protonmail.com>
2024-06-29 11:39:19 +0200

    nvim: get rid of useless alpha options

--- .config/nvim/lua/plugins/startscreen.lua
+++ .config/nvim/lua/plugins/startscreen.lua
@@ -35,7 +35,6 @@ M.config = function()
             align_shortcut = "right",
             hl_shortcut    = hl,
             hl             = texthl or hl,
-
             keymap         = { "n", bind, command }
         }
 
@@ -245,23 +244,6 @@ M.config = function()
             history,
         },
         opts = {
-            margin = 4,
-            setup = function()
-                vim.wo.cursorline = true
-                vim.wo.cursorlineopt = "line"
-                vim.wo.number = false
-                vim.wo.signcolumn = "no"
-
-                vim.api.nvim_create_autocmd("User", {
-                    pattern = "AlphaClosed",
-                    callback = function()
-                        vim.wo.cursorline = true
-                        vim.wo.cursorlineopt = "number"
-                        vim.wo.number = true
-                        vim.wo.signcolumn = "yes"
-                    end
-                })
-            end
         }
     }
     vim.cmd("Alpha")
