da9b66bcc4674fad1947d5fc0c385238523e7c32
dmxk062 <mxk06@protonmail.com>
2024-07-04 19:50:38 +0200

    eww: new nautilus window

--- .config/eww/shell/desktop/mods/nautilus.sh
+++ .config/eww/shell/desktop/mods/nautilus.sh
@@ -4,7 +4,7 @@ eww -c $XDG_CONFIG_HOME/eww/shell close rc_popup
 read -r x y h w <<< "$(slurp -w 0 -b "#4c566acc" -s "#ffffff00" -f "%x %y %h %w")"
 [[ "$x" == "" ]]&&exit
 
-(sleep 0.1&&nautilus & disown) & disown
+(sleep 0.1&&nautilus -w & disown) & disown
 socat "/run/user/$UID/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" -|while IFS=">" read -r event rest; do
     if [[ "$event" == "openwindow" ]]; then
         if [[ "$rest" == *"org.gnome.Nautilus"* ]]; then
