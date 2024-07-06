05c1fa6d33080b50694564f9b7a50098c6459c8b
dmxk062 <mxk06@protonmail.com>
2024-07-06 14:05:31 +0200

    nvim: comment

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -11,31 +11,30 @@ utils.map("i", "<M-k>", "<esc>k")
 utils.map("i", "<M-j>", "<esc>j")
 
 
-local winleader = ","
+local tableader = ","
+
 -- tabs 1 - 9
 for i = 1, 9 do
-    utils.map("n", winleader .. i, i .. "gt", { silent = true })
+    utils.map("n", tableader .. i, i .. "gt", { silent = true })
 end
 
-utils.map("n", winleader .. "h", "<cmd>tabprevious<cr>")
-utils.map("n", winleader .. "l", "<cmd>tabnext<cr>")
-utils.map("n", winleader .. "t", ":tabnew ")
+utils.map("n", tableader .. "h", "<cmd>tabprevious<cr>")
+utils.map("n", tableader .. "l", "<cmd>tabnext<cr>")
+utils.map("n", tableader .. "t", ":tabnew ")
 
-utils.map("n", winleader .. "v", ":vsp ")
-utils.map("n", winleader .. "s", ":sp ")
+utils.map("n", tableader .. "v", ":vsp ")
+utils.map("n", tableader .. "s", ":sp ")
 
 
 
 -- use <space>@ for macros instead, i dont use them that often
 utils.map("n", "<space>@", "q", {})
+
 -- faster to exit
 utils.map("n", "q", "<cmd>q<CR>")
 utils.abbrev("c", "Q", "q!")
 
--- shortcuts for some stuff i toggle often
--- utils.abbrev("c", "mo", "set mouse=a")
--- utils.abbrev("c", "mf", "set mouse=")
-
+-- shortcuts to enable/disable spelling
 utils.abbrev("c", "spen", "setlocal spell spelllang=en_us")
 utils.abbrev("c", "spde", "setlocal spell spelllang=de_at")
 utils.abbrev("c", "spoff", "setlocal spell& spelllang&")
@@ -43,24 +42,12 @@ utils.abbrev("c", "spoff", "setlocal spell& spelllang&")
 -- open a shell in a kitty window of some kind
 -- works even for remote oil buffers via ssh
 local shellleader = "<space>s"
-utils.map("n", shellleader .. "w", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window")
-end)
-utils.map("n", shellleader .. "v", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window", {location = "vsplit"})
-end)
-utils.map("n", shellleader .. "s", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window", {location = "hsplit"})
-end)
-utils.map("n", shellleader .. "W", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "os-window")
-end)
-utils.map("n", shellleader .. "t", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "tab")
-end)
-utils.map("n", shellleader .. "o", function()
-    utils.kitty_shell_in(vim.fn.expand("%:p:h"), "overlay")
-end)
+utils.map("n", shellleader .. "w", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window") end)
+utils.map("n", shellleader .. "v", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window", {location = "vsplit"}) end)
+utils.map("n", shellleader .. "s", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "window", {location = "hsplit"}) end)
+utils.map("n", shellleader .. "W", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "os-window") end)
+utils.map("n", shellleader .. "t", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "tab") end)
+utils.map("n", shellleader .. "o", function() utils.kitty_shell_in(vim.fn.expand("%:p:h"), "overlay") end)
 
 -- evaluate lua and insert result, expr=true needed for repeat
 utils.map("n", "<space>el", utils.insert_eval_lua, {expr = true})
@@ -75,15 +62,11 @@ end
 
 -- numbers
 -- without periods and minus
-utils.map({"x", "o"}, "in", function()
-    textobj.pattern("%d+")
-end)
+utils.map({"x", "o"}, "in", function() textobj.pattern("%d+") end)
 -- with periods and minus
-utils.map({"x", "o"}, "an", function()
-    textobj.pattern("%-?%d*%.?%d+")
-end)
+utils.map({"x", "o"}, "an", function() textobj.pattern("%-?%d*%.?%d+") end)
 
--- utils.map("n", "<space><space>", require("bufferswitcher").run)
+-- move between diagnostics, shortcuts for textobjects are in ./plugins/lspconfig.lua
 utils.map("n", "<space>d", vim.diagnostic.open_float)
 utils.map({"n", "x", "o"}, "[d", vim.diagnostic.goto_prev)
 utils.map({"n", "x", "o"}, "]d", vim.diagnostic.goto_next)
