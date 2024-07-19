02f1656ba4d3881a69f6f357f342fb49a4159645
dmxk062 <mxk06@protonmail.com>
2024-07-19 18:24:05 +0200

    nvim: increase treesitter file size limit to 500k

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -109,7 +109,7 @@ M.config = function()
                     end
                 end
 
-                local max_filesize = 100 * 1024
+                local max_filesize = 500 * 1024
                 local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
                     return true
