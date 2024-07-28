68358c266458ca163cccab46b1c5102d04c0bef6
dmxk062 <mxk06@protonmail.com>
2024-07-28 17:36:25 +0200

    eww: upd

--- .config/eww/shell/desktop/mods/term.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-program="$*"
-eww -c $XDG_CONFIG_HOME/eww/shell close rc_popup
-read -r x y h w < <(slurp -w 0 -b "#4c566acc" -s "#ffffff00" -f "%x %y %h %w")
-[[ "$x" == "" ]]&&exit
-
-hyprctl dispatch exec "[float; size $w $h; move $x $y]kitty" "$program"
