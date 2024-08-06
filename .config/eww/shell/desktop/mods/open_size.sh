a2f9cc6ee4e81f89a6acd84a7fcd764c2e2532eb
dmxk062 <mxk06@protonmail.com>
2024-08-06 10:26:15 +0200

    eww: upd

--- .config/eww/shell/desktop/mods/open_size.sh
+++ .config/eww/shell/desktop/mods/open_size.sh
@@ -2,12 +2,11 @@
 command="$1"
 class="$2"
 cd||exit
-eww -c $XDG_CONFIG_HOME/eww/shell close rc_popup
 
 read -r x y h w < <(slurp -w 0 -b "#4c566acc" -s "#ffffff00" -f "%x %y %h %w")
 [[ "$x" == "" ]]&&exit
 
-($command & disown) & disown
+($command & disown)
 socat "/run/user/$UID/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - | while IFS=">" read -r event rest; do
     if [[ "$event" == "openwindow" ]]; then
         if [[ "$rest" == *"$class"* ]]; then
