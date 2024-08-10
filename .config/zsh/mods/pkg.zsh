13a30337cd903c2bcbf4c425af68402e93290079
dmxk062 <mxk06@protonmail.com>
2024-08-10 18:19:06 +0200

    zsh: add flatpak wrapper script

--- .config/zsh/mods/pkg.zsh
+++ .config/zsh/mods/pkg.zsh
@@ -1,10 +1,9 @@
 #!/bin/false
 # vim: ft=zsh
 
-# simple math things i use farely often
-
 if [[ "$1" == "load" ]]; then
 
+# check which package a file belongs to, first tries to find it in $PATH
 which_pkg(){
     local progpath
     if [[ -f "$1" ]]; then
@@ -39,6 +38,9 @@ lspkg(){
 
     local package="$1"
     local files=($(pacman -Qql "$package"))
+    if [[ "$files" == "" ]]; then
+        return 1
+    fi
     local file
     for file in $files; do
         case $searchtype in
@@ -57,41 +59,6 @@ lspkg(){
                 ;;
         esac
     done
-}
-
-# flatpak wrapper for ease of use
-flat(){
-    local command="${1:-upd}"
-    local action
-    local search(){
-        local name desc id ver branch repo
-        flatpak search "$@"|sed 's/\t/;/g'| while read -r line; do
-            IFS=";" read -r name desc id ver branch repo <<< "$line"
-            # local url="$(printf '\e]8;;%s/%s\e\\%s\e]8;;\e\\\n' "https://flathub.org/apps" "$id" "$id")"
-            print -Pf '%s/%s %s\n    %s\n' "%B%F{magenta}${repo}%f"  "$name" "%F{green}${ver}%f%b" "$desc"
-        done
-    }
-    case $command in
-        '-Syu'|upd*)
-            flatpak update
-            ;;
-        '-S'|in*)
-            shift
-            flatpak install "$@"
-            ;;
-        '-R'|rm|rem*)
-            shift
-            flatpak remove "$@"
-            ;;
-        '-Ss'|search)
-            shift
-            search "$@"
-            ;;
-        *)
-            search "$@"
-            ;;
-    esac
-
 }
 
 
