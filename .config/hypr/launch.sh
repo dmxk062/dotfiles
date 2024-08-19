eebfc150c9ce560a906791331dbfa3b9032d2ac6
dmxk062 <mxk06@protonmail.com>
2024-08-19 10:01:22 +0200

    hypr: update

--- .config/hypr/launch.sh
+++ .config/hypr/launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# swww init
-# sleep 0.5
-# $HOME/.config/background/setwallpaper
+
 $HOME/.config/background/wallpaper.sh set
 $HOME/.config/hypr/eww.sh & disown
+
 killall udiskmon 
 $XDG_CONFIG_HOME/eww/shell/dock/bin/nwg_drawer.sh --initial& disown 
 $HOME/.config/swaync/diskmon.sh & disown #script to notify about inserted usbs
+
 swayidle -w& disown #idle daemon for lockscreen & suspending
 killall playerctld 
 playerctld& disown #allows me to control mpris stuff with keybinds
