d16f116075bdc8991dea41fdf414a448dd1dd6f1
dmxk062 <mxk06@protonmail.com>
2024-08-29 10:02:29 +0200

    nvim: upd

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -1,27 +1,28 @@
 local M = {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
 }
 
-M.config = function()
-    require("ibl").setup {
-        indent = {
-            highlight = {
-                "IndentBlanklineChar"
-            },
+M.opts = {
+    indent = {
+        highlight = {
+            "IndentBlanklineChar"
         },
-        scope = {
-            show_end = false,
-            highlight = "IndentBlanklineCharActive",
-            include = {
-                node_type = {
-                    lua = {
-                        "return_statement",
-                        "table_constructor"
-                    },
-                }
+    },
+    scope = {
+        show_end = false,
+        highlight = {
+            "IndentBlanklineCharActive",
+        },
+        include = {
+            node_type = {
+                lua = {
+                    "return_statement",
+                    "table_constructor"
+                },
             }
         }
     }
-end
+}
 
 return M
