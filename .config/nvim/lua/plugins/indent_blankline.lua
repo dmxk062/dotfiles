bba2a89f57dc42449865af12b5ccafcf2017b435
dmxk062 <mxk06@protonmail.com>
2024-08-29 10:18:29 +0200

    nvim: get rid of ibl hl again, too distractng

--- .config/nvim/lua/plugins/indent_blankline.lua
+++ .config/nvim/lua/plugins/indent_blankline.lua
@@ -27,9 +27,9 @@ M.config = function()
     }
 
     ---@Hack, should be replaced by a better solution at some point
-    for i=1, 5 do
-        vim.api.nvim_set_hl(0, "@ibl.scope.underline." .. tostring(i), {link = "TreesitterContext"})
-    end
+    -- for i=1, 5 do
+    --     vim.api.nvim_set_hl(0, "@ibl.scope.underline." .. tostring(i), {link = "TreesitterContext"})
+    -- end
 end
 
 return M
