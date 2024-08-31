73575c99ee07305a0cd7f83da3d5fd8263038ec0
dmxk062 <mxk06@protonmail.com>
2024-08-31 14:11:41 +0200

    nvim: improve conform cfg for jq, add shfmt

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -17,7 +17,8 @@ M.opts = {
     formatters_by_ft = {
         lua  = { "stylua" },
         c    = { "clang-format" },
-        json = { "jq" }
+        json = { "jq" },
+        bash = { "shfmt" },
     },
     default_format_opts = {
         lsp_format = "fallback"
@@ -25,8 +26,19 @@ M.opts = {
     formatters = {
         jq = {
             inherit = true,
-            -- make jq conform to vims prefered tab width
-            append_args = { "--indent", tostring(vim.o.shiftwidth) },
+            -- make jq respect shiftwidth
+            append_args = function(_, ctx)
+                if vim.bo[ctx.buf].expandtab then
+                    local width = ctx.shiftwidth
+                    if width > 7 then
+                        vim.notify("Jq only supports up to 7 spaces indent", vim.log.levels.WARN)
+                        width = 7
+                    end
+                    return { "--indent", tostring(width) }
+                else
+                    return { "--tab" }
+                end
+            end
         }
     }
 }
