a1134663d084d94aa45da30ba1d92a4ff887c4ba
dmxk062 <mxk06@protonmail.com>
2024-09-03 15:43:32 +0200

    zsh: rewrite structured data module

--- .config/zsh/mods/structured_data.zsh
+++ .config/zsh/mods/structured_data.zsh
@@ -1,169 +1,63 @@
 #!/bin/false
 # vim: ft=zsh
 
-
-# a lot of general json and other data structure functions
+# general purpose data transformation functions
+# formats:
+# table: a list of newline delimitted entries with fixed, delimiter based fields
+# hash: a built in zsh hashtable
+# json: arbitrary json arrays or maps
 
 if [[ "$1" == "load" ]]; then
 
-# two table handling functions
-# a "table" is a list of newline separated values representing objects
-# they may be preceded by a header containing the field names
-# the separator between the fields is taken as a space " " by default
-# both this and json2table below take an option sep="str" as the first argument for setting a **single** character separator
-# only simple string/int/null values are supported, no nested arrays/objects
-# example:
-# table2json sep=: login password userid groupid username home shell < /etc/passwd
-# to get all the users as json, here we need to name the fields ourselves
-function table2json {
-    local sep=" "
-    local separate=0
-    if [[ "$1" == "sep="* ]] {
-        IFS='=' read -r _ sep <<< "$1"
-        separate=1
-        shift
-    }
-    local -a columns
-
-    if (($# == 0)) {
-        if ((separate == 1)){
-            IFS="$sep" read -rA columns
-        } else {
-            read -rA columns
-        }
-
-    } else {
-        columns=("$@")
-    }
-    if ((separate == 1)){
-        column -tJ --table-columns="${(j:,:)columns}" -s "$sep" \
-            |jq -cM '.table'
-
-    } else {
-        column -tJ --table-columns="${(j:,:)columns}" \
-            |jq -cM '.table'
-    }
-}
-
-function json2table {
-    local line 
-    local sep=" "
-    if [[ "$1" == "sep="* ]] {
-        IFS='=' read -r _ sep <<< "$1"
-        shift
-    }
-    local -a columns 
-
-    local jsonObj="" 
-
-    while read -r line; do
-        jsonObj+="$line"
-    done
-
-    if (($# == 0)) {
-        columns=($(echo "$jsonObj" | jq -r '.[0]|keys_unsorted[]'))
-    } else {
-        columns=("$@")    
-    }
-    eval "print -- \${(j(${sep}))columns[@]}"
-    echo "$jsonObj" \
-        |jq --arg sep "$sep" -r '.[]|[.[] ]|join($sep)'
-    
-}
-
-# how to use:
-# json2hash myArray <<< "$(curl ipinfo.io)"
-# print $myArray[ip]
-# spaces from keys will be stripped
+# read the json map on stdin into a hashtable variable
+# dont expand child elements
+# $1: tablename
 function json2hash {
-    local arrayName="${1}"
-    declare -gA "$arrayName"
-    local jsonObj key value
+    local array_name="$1"
+    declare -gA "$array_name"
+    local json_object key value
 
-    jq -rMc 'to_entries| map("\(.key)\t\(.value)")| .[]' \
-        |while IFS=$'\t' read -r key value; do
-        eval $arrayName'[$key]="$value"'
-    done
+    jq -rMc 'to_entries|map("\(.key)\t\(.value)")|.[]'\
+        | while IFS=$'\t' read -r key value; do
+            eval "$array_name"'[$key]="$value"'
+        done
 }
 
-function json2array {
-    local arrayName="${1}"
-    declare -ga "$arrayName"
-    local line jsonObj value
-
-    while read -r line; do
-        jsonObj+="$line"
-    done
-
-    echo "$jsonObj"|jq -rMc '.[]' \
-        |while read -r value; do
-        eval $arrayName'+="$value"'
-    done
+# $1: tablename
+function hash2json {
+    local array_name="$1" key value
+    for key value in "${(@kv)${(P)array_name}}"; do
+        printf "%s\t%s\n" "$key" "$value"
+    done|jq -RMs 'split("\n")[:-1]|map(split("\t"))|map({(.[0]): .[1]})|add'
 }
 
-# nice way to deserialize too
-function list2hash {
-    local arrayName="${1}"
-    declare -gA "$arrayName"
-    local line value
+# $1: separator, empty string for IFS
+# $@:2: names for columns, if not given first row of stream will be used
+function table2json {
+    local sep="$1"
+    local separate=0
+    shift
 
-    while read -r key value; do
-        eval $arrayName'[$key]="$value"'
-    done
-}
+    if [[ "$sep" != "" ]]; then
+        separate=1
+    fi
 
-
-# returns all the assigned lists' names in the first parameter
-function table2lists {
     local -a columns
-    local output_name="$1"; shift
-    local num_columns column line
-    
 
-    if (($# == 0)) {
-        read -rA columns
-    } else {
+    if (($# <= 1)); then
+        IFS="$sep" read -rA columns
+    else 
         columns=("$@")
-    }
-    num_columns=$#columns 
+    fi
 
-    for column in "${columns[@]}"; do
-        declare -ga "$column"
-    done
-
-    while read -rA line; do
-        for ((i=1; i<=num_columns; i++)) {
-            eval ${columns[i]}+='${line[i]}'
-        }
-    done
-    declare -ga "$output_name"
-    eval "$output_name"='("${columns[@]}")'
+    if ((separate)); then
+        column -tJ --table-columns="${(j:,:)columns}" -s "$sep"|jq -cM '.table'
+    else
+        column -tJ|jq -cM '.table'
+    fi
 }
-
-# index a named array, very useful with the above
-# e.g. IFS=: table2lists etc_passwd login passwd uid gid name home shell < /etc/passwd
-# iindex etc_passwd 1,5 users
-function iindex {
-    local arrayName="$1"
-    local index="$2"
-
-    if (($# == 3)){
-        eval "$3"="(\"\$${arrayName}[$index]\")"
-    } else {
-        eval "print -- \"\$${arrayName}[$index]\""
-    }
-}
-
-alias jsoniter="jq -Mc '.[]'"
 
 elif [[ "$1" == "unload" ]]; then
-
-unfunction table2json json2table \ 
-    json2hash json2array \
-    list2hash \
-    iindex
-
-unalias jsoniter
-
-
+    unfunction json2hash hash2json \
+        table2json
 fi
