7190cc6f527327efda62e90d14582715f8604288
dmxk062 <mxk06@protonmail.com>
2024-09-06 15:57:44 +0200

    zsh: add dynamic "directories" containing the clipboard

--- .config/zsh/handlers.zsh
+++ .config/zsh/handlers.zsh
@@ -46,3 +46,85 @@ function __readnullcommand {
 }
 
 READNULLCMD=__readnullcommand
+
+
+# abuse for the clipboard, can contain any data, not just a dir
+function _clipboard_directory_name {
+    if [[ "$1" == "d" ]]; then
+        return 1
+    fi
+
+    if [[ "$1" == "n" ]]; then
+        if [[ "$2" != "clip"* ]] && [[ "$2" != "sel"* ]]; then
+            return 1
+        fi
+
+        local cmd="wl-paste"
+        local content err type mime use_mime
+        if [[ "$2" == *":"* ]]; then
+            IFS=":" read -r type mime <<< "$2"
+            use_mime=1
+        else
+            type="$2"
+        fi
+
+        if [[ "$type" == "clip" ]]; then
+            if ((use_mime)); then
+                local -a mime_types=($(wl-paste -l 2>/dev/null))
+                if ! (($mime_types[(I)$mime])); then
+                    return 1
+                fi
+                content="$(wl-paste --type "$mime" 2>/dev/null)"
+                err=$?
+            else
+                content="$(wl-paste 2>/dev/null)"
+                err=$?
+            fi
+        elif [[ "$type" == "sel" ]]; then
+            if ((use_mime)); then
+                local -a mime_types=($(wl-paste --primary -l 2>/dev/null))
+                if ! (($mime_types[(I)$mime])); then
+                    return 1
+                fi
+                content="$(wl-paste --primary --type "$mime" 2>/dev/null)"
+                err=$?
+            else
+                content="$(wl-paste --primary 2>/dev/null)"
+                err=$?
+            fi
+        fi
+
+        if ((err > 0)) || [[ -z "$content" ]]; then
+            return 1
+        fi
+
+        typeset -ga reply
+        reply=("$content")
+        return 0
+    fi
+
+    if [[ "$1" == "c" ]]; then
+        local primary_types secondary_types primary_err secondary_err
+        primary_types=($(wl-paste --primary -l 2>/dev/null))
+        primary_err=$?
+        secondary_types=($(wl-paste -l 2>/dev/null))
+        secondary_err=$?
+
+        local -a compls
+        if ! ((primary_err)); then
+            compls+=("sel")
+            compls+=("${(@)primary_types/#/"sel:"}")
+        fi
+
+        if ! ((secondary_err)); then
+            compls+=("clip")
+            compls+=("${(@)secondary_types/#/"clip:"}")
+        fi
+
+        _wanted dynamic-dirs expl 'clipboards' compadd -S\] -a compls
+    fi
+
+
+}
+
+zsh_directory_name_functions+=(_clipboard_directory_name)
