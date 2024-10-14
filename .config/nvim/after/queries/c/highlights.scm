7e22361eadac2063598a5a1fe4bb1e0887111537
dmxk062 <mxk06@protonmail.com>
2024-10-14 18:26:24 +0200

    nvim: remove typecast query

--- .config/nvim/after/queries/c/highlights.scm
+++
@@ -1,7 +0,0 @@
-; extends
-; ((cast_expression
-;  value: (identifier) @variable.typecast)
-;     (#set! priority 1000))
-((cast_expression
- type: (type_descriptor) @type.typecast)
-    (#set! priority 1000))
