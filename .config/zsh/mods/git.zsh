228514a52ed2f44bea8420c10bebf1466e828ae1
dmxk062 <mxk06@protonmail.com>
2024-10-19 09:34:31 +0200

    zsh: improve lschg function

--- .config/zsh/mods/git.zsh
+++ .config/zsh/mods/git.zsh
@@ -4,34 +4,53 @@
 if [[ "$1" == "load" ]] {
 
 function lschg {
-    local dir="${1:-.}"
     local all=0
-    local hide="${2}"
+    local -a dirs
     local ignored=no
-    if [[ "$hide" == "-a" ]] || [[ "$hide" == "--all" ]]; then
-        all=1
-        ignored="traditional"
+    for arg in "$@"; do
+        if [[ "$arg" == "-a" || "$arg" == "--all" ]]; then
+            all=1
+            ignored=traditional
+        else 
+            dirs+=("$arg")
+        fi
+    done
+
+    function list_changes {
+        local line mode file prefix
+        while read -r type line; do
+            if [[ $type == 1 ]]; then
+                read -r mode _ _ _ _ _ _ file <<< "$line";
+                case $mode in 
+                    \.M) prefix="%F{magenta}";;
+                    M\.) prefix="%B%F{magenta}";;
+                    MM)  prefix="%B%F{magenta}";;
+                    A\.) prefix="%B%F{green}";;
+                    \.A) prefix="%F{greem}";;
+                    AA) prefix="%B%F{green}";;
+                    D\.) prefix="%B%F{red}";;
+                    \.D) prefix="%F{red}";;
+                    DD) prefix="%B%F{red}";;
+                esac
+                print -P -- "$prefix$mode $file%f%b"
+            elif [[ "$type" == '?' || "$type" == "!" ]] && ((all)); then
+                print -P -- "\e[90m$type  $line%f"
+            fi
+            # HACK: cd so we can work with *any* git repo
+        done < <(cd -- "$1"; git status --porcelain=v2 --ignored="$ignored" ".")
+    }
+
+    if (($#dirs == 0)); then
+        list_changes "."
+    elif (($#dirs == 1)); then
+        list_changes "${dirs[1]}"
+    else
+        for dir in "${dirs[@]}"; do
+            print -- "$dir:"
+            list_changes "$dir"
+        done
     fi
-    local line mode file prefix
-    while read -r type line; do
-        if [[ $type == 1 ]] {
-            read -r mode _ _ _ _ _ _ file <<< "$line";
-            case $mode in 
-                \.M) prefix="%F{magenta}";;
-                M\.) prefix="%B%F{magenta}";;
-                MM)  prefix="%B%F{magenta}";;
-                A\.) prefix="%B%F{green}";;
-                \.A) prefix="%F{greem}";;
-                AA) prefix="%B%F{green}";;
-                D\.) prefix="%B%F{red}";;
-                \.D) prefix="%F{red}";;
-                DD) prefix="%B%F{red}";;
-            esac
-            print -P -- "$prefix$mode $file%f%b"
-        } elif [[ "$type" == '?' || "$type" == "!" ]] && ((all)) {
-            print -P -- "\e[90m$type  $line%f"
-        }
-    done < <(git status --porcelain=v2 --ignored="$ignored" "$dir")
+
 }
 
 alias lgit="lsd -l --config-file $HOME/.config/lsd/brief.yaml" \
