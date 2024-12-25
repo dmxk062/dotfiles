ce4c15a7a562b8e5890dc7850002fce0abdc8bb1
dmxk062 <mxk06@protonmail.com>
2024-12-25 17:20:48 +0100

    nvim: better alignment for vim.ui.input from dressing

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -5,15 +5,20 @@ return {
             insert_only = false,
             default_prompt = "Input",
             trim_prompt = false,
+            relative = "win",
+            override = function(opts)
+                opts.col = 1
+                return opts
+            end,
         },
         select = {
             enabled = true,
             backend = { "builtin", "telescope" },
             builtin = {
-                max_height = {12, 0},
-                min_height = {4,  0},
+                max_height = { 12, 0 },
+                min_height = { 4, 0 },
                 relative = "cursor",
-                override = function (opts)
+                override = function(opts)
                     opts.row = opts.row + 1
                     return opts
                 end,
