b95478e4e42463b85e2ee0411d33e163474dab6b
dmxk062 <mxk06@protonmail.com>
2024-12-25 12:18:28 +0100

    nvim: move luarocks into dependencies.lua

--- .config/nvim/lua/plugins/dependencies.lua
+++ .config/nvim/lua/plugins/dependencies.lua
@@ -1,4 +1,14 @@
 return {
     { "nvim-lua/plenary.nvim",       lazy = true },
     { "nvim-tree/nvim-web-devicons", lazy = true },
+    {
+        "vhyrro/luarocks.nvim",
+        lazy = true,
+        cmd = { "RocksInstall" },
+        opts = {
+            rocks = {
+                "magick"
+            }
+        }
+    }
 }
