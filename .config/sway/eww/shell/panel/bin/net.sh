3d68a7ad9df602279caa75854e06b275eadecb58
dmxk062 <mxk06@protonmail.com>
2024-12-30 23:14:33 +0100

    eww: increase polling frequency for network

--- .config/sway/eww/shell/panel/bin/net.sh
+++ .config/sway/eww/shell/panel/bin/net.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DEV="$(ip route | grep default | cut -d' ' -f5)"
 
-sar -n DEV 4 --iface="$DEV" | while read -r _ _ ident _ _ recvd sent _; do
+sar -n DEV 2 --iface="$DEV" | while read -r _ _ ident _ _ recvd sent _; do
     if [[ "$ident" == "$DEV" ]]; then
 	nice_sent="$(numfmt --from=iec --to=iec "${sent}K")"
 	nice_recvd="$(numfmt --from=iec --to=iec "${recvd}K")"
