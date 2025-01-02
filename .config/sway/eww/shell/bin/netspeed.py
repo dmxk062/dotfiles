af48b08efc27c1bd9255ff8269e2819cc57db18d
dmxk062 <mxk06@protonmail.com>
2025-01-02 14:07:48 +0100

    eww: better network statistics

---
+++ .config/sway/eww/shell/bin/netspeed.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import psutil
+import time
+import json
+import sys
+
+UPPER_SENT=20 * 1024 * 1024
+UPPER_RECV=30 * 1024 * 1024
+
+sizes = ["B", "KB", "MB", "GB", "TB"]
+
+def format_size(byte_value):
+    for unit in sizes:
+        if byte_value < 1024:
+            return f"{byte_value:.1f}{unit}"
+        byte_value /= 1024
+
+while True:
+    net_stats_1 = psutil.net_io_counters()
+    time.sleep(1)
+    net_stats_2 = psutil.net_io_counters()
+
+    diff_sent = net_stats_2[0] - net_stats_1[0]
+    diff_recv = net_stats_2[1] - net_stats_1[1]
+    out = {
+        "total_sent": net_stats_2[0],
+        "total_sent_pretty": format_size(net_stats_2[0]),
+        "total_recv": net_stats_2[1],
+        "total_recv_pretty": format_size(net_stats_2[1]),
+        "sent_per_sec": diff_sent,
+        "sent": diff_sent,
+        "sent_pretty": format_size(diff_sent),
+        "recv": diff_recv,
+        "recv_pretty": format_size(diff_recv),
+        "sent_rel": (diff_sent / UPPER_SENT) * 100,
+        "recv_rel": (diff_recv / UPPER_RECV) * 100,
+    }
+
+    json.dump(out, fp=sys.stdout)
+    sys.stdout.write("\n")
+    sys.stdout.flush()
