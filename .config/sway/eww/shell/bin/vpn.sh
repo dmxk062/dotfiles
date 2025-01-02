a6e9b3b2ccd58106ac4b0a9adf6f82a25c1a01a2
dmxk062 <mxk06@protonmail.com>
2025-01-02 14:08:01 +0100

    fix(eww): add missing vpn script

---
+++ .config/sway/eww/shell/bin/vpn.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+EWW="$XDG_CONFIG_HOME/sway/eww/shell"
+
+function get_active {
+    nmcli -g TYPE,ACTIVE,UUID,NAME connection show \
+        |awk -F":" '
+            $1 == "wireguard" && $2 == "yes" {
+                printf "{\"active\": true, \"name\":\"%s\", \"uuid\":\"%s\"}\n",$4,$3;
+                fflush();
+                exit 1;
+            }' && echo '{"active": false, "name": "", "uuid": ""}'
+}
+
+function list_vpns {
+    nmcli -g TYPE,ACTIVE,UUID,NAME connection show \
+        |while IFS=":" read -r type active uuid name; do
+            if [[ "$type" != "wireguard" ]]; then
+                continue
+            fi
+
+            if [[ "$active" == "yes" ]]; then
+                bactive=true
+            else
+                bactive=false
+            fi
+
+            printf '{"active":%s,"name":"%s","uuid":"%s"}\n' \
+                "$bactive" "$name" "$uuid"
+            done|jq -s 'sort_by(.name)'
+}
+
+case "$1" in
+    listen)
+        get_active
+        nmcli monitor | while read -r _; do get_active; done 
+        ;;
+    list)
+        list_vpns
+        ;;
+    upd)
+        eww -c "$EWW" update vpns="$(list_vpns)"
+        ;;
+esac
