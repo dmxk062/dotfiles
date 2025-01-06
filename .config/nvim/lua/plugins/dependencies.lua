18993b93cb797804eb584314ff6fa0ec3ec7752f
dmxk062 <mxk06@protonmail.com>
2025-01-06 16:00:05 +0100

    nvim: get rid of neorg

--- .config/nvim/lua/plugins/dependencies.lua
+++ .config/nvim/lua/plugins/dependencies.lua
@@ -1,14 +1,4 @@
 return {
     { "nvim-lua/plenary.nvim",       lazy = true },
     { "nvim-tree/nvim-web-devicons", lazy = true },
-    {
-        "vhyrro/luarocks.nvim",
-        lazy = true,
-        cmd = { "RocksInstall" },
-        opts = {
-            rocks = {
-                "magick"
-            }
-        }
-    }
 }
