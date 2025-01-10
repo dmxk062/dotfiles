783999959c2df5c2161dd7ebc5b3d726d445bad2
dmxk062 <mxk06@protonmail.com>
2025-01-10 16:38:20 +0100

    nvim: improve bash queries

--- .config/nvim/after/queries/bash/injections.scm
+++ .config/nvim/after/queries/bash/injections.scm
@@ -1,31 +1,52 @@
 ; extends
 
+; Inject jq code
 (command
-   name: (command_name) @_command
-   argument : [
-               (string 
-                 (string_content) @injection.content)
-               (concatenation 
-                 (string
-                   (string_content) @injection.content))
-               (raw_string) @injection.content
-               (concatenation 
-                 (raw_string) @injection.content)
-               ]
- (#eq? @_command "jq")
- (#set! injection.language "jq"))
+  name: (command_name) @_command
+  argument: [
+    (string
+      (string_content) @injection.content)
+    (concatenation
+      (string
+        (string_content) @injection.content))
+    (raw_string) @injection.content
+    (concatenation
+      (raw_string) @injection.content)
+  ]
+  (#eq? @_command "jq")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "jq"))
 
+; Basically the same for awk, but also do it for all forms of awk
 (command
-   name: (command_name) @_command
-   argument : [
-               (string 
-                 (string_content) @injection.content)
-               (concatenation 
-                 (string
-                   (string_content) @injection.content))
-               (raw_string) @injection.content
-               (concatenation 
-                 (raw_string) @injection.content)
-               ]
- (#eq? @_command "awk")
- (#set! injection.language "awk"))
+  name: (command_name) @_command
+  argument: [
+    (string
+      (string_content) @injection.content)
+    (concatenation
+      (string
+        (string_content) @injection.content))
+    (raw_string) @injection.content
+    (concatenation
+      (raw_string) @injection.content)
+  ]
+  (#any-of? @_command "awk" "gawk" "nawk")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "awk"))
+
+; Languages that take a -c argument
+((command
+  name: (command_name) @_command @injection.language
+  argument: (word) @_arg
+  argument: [
+    (string) @injection.content
+    (concatenation
+      (string) @injection.content)
+    (raw_string) @injection.content
+    (concatenation
+      (raw_string) @injection.content)
+  ])
+  (#any-of? @_command "sh" "bash" "python")
+  (#eq? @_arg "-c")
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.include-children))
