c7be6a62fa4795053b2339325b8d753f211284c7
dmxk062 <mxk06@protonmail.com>
2025-01-13 09:45:45 +0100

    nvim: use proper cursorlineopt for undotree

--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -6,15 +6,29 @@ M.config = function()
     local utils = require("utils")
     utils.map("n", "<space>u", vim.cmd.UndotreeToggle)
 
-    vim.g.undotree_ShortIndicators = true
-    vim.g.undotree_WindowLayout = 2
-    vim.g.undotree_SetFocusWhenToggle = true
-    vim.g.undotree_DiffAutoOpen = false
-    vim.g.undotree_HelpLine = false
-    vim.g.undotree_TreeNodeShape = "│"
-    vim.g.undotree_TreeVertShape = "╷"
-    vim.g.undotree_TreeSplitShape = "⟋"
-    vim.g.undotree_TreeReturnShape = "⟍"
+    local g = vim.g
+
+    g.undotree_ShortIndicators = true
+    g.undotree_WindowLayout = 2
+    g.undotree_SetFocusWhenToggle = true
+    g.undotree_DiffAutoOpen = false
+    g.undotree_HelpLine = false
+    g.undotree_TreeNodeShape = "│"
+    g.undotree_TreeVertShape = "╷"
+    g.undotree_TreeSplitShape = "⟋"
+    g.undotree_TreeReturnShape = "⟍"
+
+    _Undotree_on_win_enter = function()
+        vim.wo[0][0].cursorlineopt = "both"
+    end
+
+    -- HACK: I couldnt figure out how to get a funcref from a v:lua call
+    -- TODO: Get rid of this vimscript
+    vim.cmd [[
+    function g:Undotree_CustomMap()
+        call v:lua._Undotree_on_win_enter()
+    endfunction
+    ]]
 end
 
 return M
