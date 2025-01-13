cc44f0fb968bfeac02eac1aa5710f21146379cd8
dmxk062 <mxk06@protonmail.com>
2025-01-13 17:14:04 +0100

    rofi: remove unused window applet

--- .config/rofi/applets/window/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-exec rofi -show window\
-    -modes "window:$XDG_CONFIG_HOME/rofi/applets/window/window_search.sh"\
-    -theme "$XDG_CONFIG_HOME/rofi/applets/window/theme.rasi"\
-    -config "$XDG_CONFIG_HOME/rofi/applets/window/config.rasi"
