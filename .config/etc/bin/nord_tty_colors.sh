6ab0a078e523fa1d0b061fd7398036fd570ed4b1
dmxk062 <mxk06@protonmail.com>
2025-01-16 09:14:23 +0100

    upd /etc/issue

---
+++ .config/etc/bin/nord_tty_colors.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+printf "]P9bf616a]P82e3440]P7eceff4]P68fbcbb]P5b48ead]P45e81ac]P3ebcb8b]P2a3be8c]P1bf616a]P02e3440]PFeceff4]PE8fbcbb]PDb48ead]PC5e81ac]PBebcb8b]PAa3be8c[3J" > /dev/tty1
+
+
