59855d9ecfc7455150ea5c64ec1e82dfc1705ec5
dmxk062 <mxk06@protonmail.com>
2025-01-16 16:54:19 +0100

    nvim: add back the more intuitive mappings to leap for remote

--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -4,18 +4,20 @@ local M = {
 
 M.config = function()
     local utils = require("utils")
-    utils.map("n", "S", "<Plug>(leap-from-window)")
-    utils.map("n", "s", "<Plug>(leap)")
+    local map = utils.map
+
+    map("n", "S", "<Plug>(leap-from-window)")
+    map("n", "s", "<Plug>(leap)")
 
     -- much more flexible than leap-spooky, no more need for mapping every object, this allows motions
     -- format is different: <op>r<leap><motion/textobject>
     -- e.g. crle<cr>i"<esc>
     -- repeat the operator for line: crle<cr>c<esc>
     -- not for visual mode, since r is useful there
-    utils.map("o", "r", function() require("leap.remote").action() end)
+    map("o", "r", function() require("leap.remote").action() end)
 
     -- use from normal mode: e.g. gR<leap>dd
-    utils.map("n", "gR", function() require("leap.remote").action() end)
+    map("n", "gR", function() require("leap.remote").action() end)
 
     -- HACK: override colors only after it has been setup
     vim.api.nvim_create_autocmd("User", {
@@ -29,6 +31,46 @@ M.config = function()
             })
         end
     })
+
+
+    -- additionally, for textobjects just
+    -- d<a,i>r<thing> works too
+    -- easier to remember, more vim
+    local textobjects_ai = {
+        -- builtins
+        "w", "W", "s", "p",
+        -- delimiters
+        "[", "]", "(", ")", "{", "}", "'", '"', "`", "<", ">", "b", "t", "q", "Q", "B",
+        -- treesitter
+        "f", "a", "c", "l", "C", "r",
+        -- my own
+        "i", "o", "_"
+    }
+
+    -- not full pairs
+    local textobjects_full = {
+        -- treesitter
+        "iv", "aA", "iN", "in",
+        -- my own
+        "id", "iDe", "iDw", "iDi", "iDh", "aI"
+    }
+
+    for _, obj in ipairs(textobjects_ai) do
+        map({ "x", "o" }, "ir" .. obj, function()
+            require("leap.remote").action { input = "i" .. obj }
+        end)
+        map({ "x", "o" }, "ar" .. obj, function()
+            require("leap.remote").action { input = "a" .. obj }
+        end)
+    end
+
+    for _, obj in ipairs(textobjects_full) do
+        local scope = obj:sub(1, 1)
+
+        map({ "x", "o" }, scope .. "r" .. obj:sub(2), function()
+            require("leap.remote").action { input =  obj }
+        end)
+    end
 end
 
 return M
