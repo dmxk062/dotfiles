2e450affc81abeb38f359b330f9d9ac99628e0fc
dmxk062 <mxk06@protonmail.com>
2025-01-30 19:08:24 +0100

    eww: fmt

--- .config/sway/eww/shell/bin/netspeed.py
+++ .config/sway/eww/shell/bin/netspeed.py
@@ -23,15 +23,14 @@ while True:
 
     diff_sent = net_stats_2[0] - net_stats_1[0]
     diff_recv = net_stats_2[1] - net_stats_1[1]
-    out = {
+
+    json.dump({
         "total_sent_pretty": format_size(net_stats_2[0]),
         "total_recv_pretty": format_size(net_stats_2[1]),
         "sent_pretty": format_size(diff_sent),
         "recv_pretty": format_size(diff_recv),
         "sent_rel": (diff_sent / UPPER_SENT) * 100,
         "recv_rel": (diff_recv / UPPER_RECV) * 100,
-    }
-
-    json.dump(out, fp=sys.stdout)
+    }, fp=sys.stdout)
     sys.stdout.write("\n")
     sys.stdout.flush()
