da8e0236c0f818a1c1118666d4fb604794c49775
dmxk062 <mxk06@protonmail.com>
2025-02-06 17:31:33 +0100

    eww: add weather to status area

--- .config/sway/eww/shell/bin/weather.sh
+++ .config/sway/eww/shell/bin/weather.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 
-CITY="graz"
+CITY="$(eww -c "$XDG_CONFIG_HOME/sway/eww/shell" get weather-city)"
 KEY=$(< "$XDG_DATA_HOME/keys/openweather")
 
-upd() {
-    eww -c "$XDG_CONFIG_HOME/sway/eww/shell/" update "$@"
-}
-
-if (( $1 > 300 )); then
-    weather="$(curl -sf "http://api.openweathermap.org/data/2.5/weather?appid=${KEY}&q=${CITY}&units=metric")"
-    upd weather="$weather"
-    upd weather-last="$(date +%s)"
-fi
+curl -sf "http://api.openweathermap.org/data/2.5/weather?appid=${KEY}&q=${CITY}&units=metric"
