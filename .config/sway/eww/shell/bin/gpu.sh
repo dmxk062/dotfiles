c4f42c5f375a8581932ada8e618d6f9c2e899aef
dmxk062 <mxk06@protonmail.com>
2025-02-07 21:26:14 +0100

    eww: eliminate initial gpu delay

--- .config/sway/eww/shell/bin/gpu.sh
+++ .config/sway/eww/shell/bin/gpu.sh
@@ -3,11 +3,12 @@
 printf -v GPU "%s" /sys/class/drm/card?
 path="$GPU/device"
 
-while sleep 3; do
+while true; do
     read -r mem_used < "$path/mem_info_vram_used"
     read -r mem_total < "$path/mem_info_vram_total"
     read -r usage < "$path/gpu_busy_percent" 
 
     printf '{"mem_used":%s, "mem_total":%s, "usage":%s}\n' \
         "$mem_used" "$mem_total" "$usage"
+    sleep 3
 done
