fdfde757abf1790b1ed203b407d960ed1b2af73a
dmxk062 <mxk06@protonmail.com>
2025-02-07 19:28:30 +0100

    rofi: switch to my own sway mode

---
+++ .config/rofi/sway.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ICON="\0icon\x1f"
+INFO="\x1finfo\x1f"
+
+if ((ROFI_RETV == 0)); then
+    echo -en "\0use-hot-keys\x1ftrue\n\0delim\x1f\t\n"
+    while IFS=$'\t' read -r id name class is_active; do
+        if ((is_active)); then
+            continue
+        fi
+
+        icon="$class"
+        if [[ "$class" == "kitty" ]]; then
+            case "$name" in
+                nv:*)
+                    icon="nvim"
+                    ;;
+                lf:*)
+                    icon="file-manager"
+                    ;;
+            esac
+        fi
+        printf "%s\n%s$ICON%s$INFO%s\t" "$name" "$class" "$icon" "$id"
+    done < <(swaymsg -t get_tree |
+        jq -r '.. | (.nodes? // empty)[] | select(.pid) | "\(.id)\t\(.name)\t\(.app_id? // .window_properties.class)\t\(if .focused then 1 else 0 end)"')
+else
+    if ((ROFI_RETV == 10)); then
+        swaymsg "[con_id=$ROFI_INFO] move to workspace current" > /dev/null
+    fi
+    swaymsg "[con_id=$ROFI_INFO] focus" > /dev/null
+fi
