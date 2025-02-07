127ee0d74f39393e3ddbdf4f99853be0ac7e4e74
dmxk062 <mxk06@protonmail.com>
2025-02-07 13:38:04 +0100

    eww: improve weather handling

--- .config/sway/eww/shell/bin/weather.sh
+++ .config/sway/eww/shell/bin/weather.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-CITY="$(eww -c "$XDG_CONFIG_HOME/sway/eww/shell" get weather-city)"
+CITY="Graz"
 KEY=$(< "$XDG_DATA_HOME/keys/openweather")
 
 curl -sf "http://api.openweathermap.org/data/2.5/weather?appid=${KEY}&q=${CITY}&units=metric"
+echo
