675bad13fc2e88258d3afcacb2ea7eb2b848dced
dmxk062 <mxk06@protonmail.com>
2025-02-08 09:26:39 +0100

    nvim: add additional surrounds

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -3,7 +3,28 @@ local M = {
     event = "VeryLazy"
 }
 
+local function generic_pair(left, right)
+    local esc_left, esc_right = vim.pesc(left), vim.pesc(right)
+    local cdelete = string.format("^(%s)().-(%s)()$", esc_left, esc_right)
+    return {
+        add = { left, right },
+        find = string.format("%s.-%s", esc_left, esc_right),
+        delete = cdelete,
+        change = {
+            target = cdelete
+        }
+    }
+end
+
 M.opts = {
+    surrounds = {
+        -- variable expansion
+        ["v"] = generic_pair("${", "}"),
+        -- subshell expansion
+        ["x"] = generic_pair("$(", ")"),
+        -- lua table key
+        ["k"] = generic_pair('["', '"]'),
+    }
 }
 
 return M
