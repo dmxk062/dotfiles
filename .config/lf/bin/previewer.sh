066b7d8b1fd3c6ecc9cce761aa5b065042e3e8e1
dmxk062 <mxk06@protonmail.com>
2025-02-15 13:26:28 +0100

    lf: adopt a much better approach to preview caching

--- .config/lf/bin/previewer.sh
+++ .config/lf/bin/previewer.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
 CACHEDIR="$XDG_CACHE_HOME/lf"
-if ! [[ -d "$CACHEDIR" ]] { mkdir -p "$CACHEDIR" }
+[[ ! -d "$CACHEDIR" ]] && mkdir -p "$CACHEDIR"
 
-
+# given to us by lf
 FILE=$1
 W=$2
 H=$3
@@ -14,6 +14,8 @@ COLUMNS="$W"
 
 IMAGE_SIZE="600x400"
 
+# $1: image to display (file)
+# $2: height offset
 function display_image {
     kitten icat --silent --stdin no --transfer-mode memory --place "${W}x$[H-${2:-0}]@${X}x$[Y+${2:-0}]" "$1" < /dev/null > /dev/tty
 }
@@ -26,52 +28,59 @@ function separator {
     printf "%.0s-" {1..$W}
 }
 
+# create a cache entry for a file
+# takes mtime into account, so changes to a file will result in different cache entries
+# $1: filepath
+# $@:2: entries to create names for
+# $out: reply
+# $return: whether the files need to be initialized
 function create_cache {
-    local pathn="${1:h4}"
-    local pathn="${pathn//\//.}"
-    local pathe="${1:t4}"
-    local pathe="${pathe//\//.}"
-    print -- "$CACHEDIR/${pathn:1}.${pathe}${2}"
+    local id="$(stat -c "%m.%i.%Y" -- "$1")"
+    id="${id//\//@}"
+
+    for ((i=2; i <= $#; i++)); do
+        reply[$i-1]="$CACHEDIR/${id}${argv[$i]}"
+    done
+    [[ ! -f "${reply[1]}" ]]
 }
 
 
 if ! [[ -r "$FILE" ]] {
     print -P "%SPermission Denied"
-    exit 1
 }
 
 MIMETYPE="$(file --dereference --brief --mime-type -- "$FILE")"
 
 case "$MIMETYPE" in
     application/pdf) 
-        tmpfile="$(create_cache "${FILE}" ".png")"
-        if ! [[ -f "$tmpfile" ]] {
-            pdftoppm -f 1 -l 1 -png "$FILE" >> "$tmpfile"
-        }
-        display_image "$tmpfile"
-        exit 1
+        if create_cache "${FILE}" .png; then
+            pdftoppm -f 1 -l 1 -png "$FILE" >> "${reply[1]}"
+        fi
+        display_image "${reply[1]}"
+        ;;
+
+    image/svg+xml)
+        if create_cache "$FILE" .png; then
+            magick -background none -size 400x400 "$FILE" "${reply[1]}" 
+        fi
+        display_image "${reply[1]}"
         ;;
 
     image/*)
-        tmpfile="$(create_cache "${FILE}")"
-        datafile="$(create_cache "$FILE" ".desc")"
-        if [[ ! -f "$tmpfile" ]] {
-            magick convert "$FILE" -resize "$IMAGE_SIZE" "$tmpfile"&
-            identify -format 'Format: %m\nResolution: %wx%h\nColor Depth: %z Bits per Pixel\nTaken by: %[EXIF:Make] %[EXIF:Model]\n' "$FILE" > "$datafile"&
+        if create_cache "${FILE}" .png .desc; then
+            magick "$FILE" -resize "$IMAGE_SIZE" "${reply[1]}"&
+            identify -format 'Format: %m\nResolution: %wx%h\nColor Depth: %z Bits per Pixel\nTaken by: %[EXIF:Make] %[EXIF:Model]\n' "$FILE" > "${reply[2]}"&
             wait
-        }
-        cat "$datafile"
-        display_image "$tmpfile" 6
-        exit 1
+        fi
+        cat "${reply[2]}"
+        display_image "${reply[1]}" 6
         ;;
 
     video/*)
-        tmpfile="$(create_cache "${FILE}" ".png")"
-        datafile="$(create_cache "${FILE}" ".desc")"
-        if ! [[ -f "$tmpfile" ]] {
+        if create_cache "${FILE}" .png .desc; then
             (
                 IFS=$'\t' read -r audio_format audio_bitrate video_format video_bitrate video_resolution video_framerate video_duration < <(mediainfo --output=JSON "$FILE" \
-                    |jq -r '([.media.track|.[]|select(."@type" == "Video")][0]) as $video | ([.media.track|.[]|select(."@type" == "Audio")][0]) as $audio|
+                    | jq -r '([.media.track|.[]|select(."@type" == "Video")][0]) as $video | ([.media.track|.[]|select(."@type" == "Audio")][0]) as $audio|
                     "\($audio.Format)\t\($audio.BitRate)\t\($video.Format)\t\($video.BitRate)\t\($video.Width)x\($video.Height)\t\($video.FrameRate)\t\($video.Duration)"'
                 )
                 seconds="${video_duration%.*}"
@@ -90,21 +99,18 @@ case "$MIMETYPE" in
                 fi
                 printf "Audio: %s\nVideo: %s (%s)\nResolution: %s\nFramerate: %sfps\nRuntime: %02d:%02d:%02d\n" \
                     "$audio" "$video_format" "$video_bitrate" "$video_resolution" "$video_framerate" "$hours" "$minutes" "$seconds" \
-                > "$datafile"
+                > "${reply[2]}"
             )&
-            ffmpegthumbnailer -s 512 -m -i "$FILE" -o "$tmpfile"&
+            ffmpegthumbnailer -s 512 -m -i "$FILE" -o "${reply[1]}"&
 
             wait
-        }
-        cat "$datafile"
-        display_image "$tmpfile" 6
-        exit 1
+        fi
+        cat "${reply[2]}"
+        display_image "${reply[1]}" 6
         ;;
 
     audio/*)
-        tmpfile="$(create_cache "${FILE}" ".png")"
-        datafile="$(create_cache "${FILE}" ".desc")"
-        if [[ ! -f "$tmpfile" ]] {
+        if create_cache "${FILE}" .png .desc; then
             (
                 IFS=$'\t' read -r title album artist genre format duration bitrate < <(mediainfo --output=JSON "$FILE" \
                     |jq -r '.media.track[0]|"\(.Title // "-")\t\(.Album // "-")\t\(.Performer // "-")\t\(.Genre // "-")\t\(.Format)\t\(.Duration)\t\(.OverallBitRate)"')
@@ -115,20 +121,19 @@ case "$MIMETYPE" in
                 local genre_str="${(j:,:)genres}"
                 printf "Title: %s\nArtist: %s\nAlbum: %s\nGenre(s): %s\nFormat: %s\nBitrate: %skbps\nDuration: %02d:%02d"  \
                     "${title}" "${artist}" "${album}" "${genre_str}" "${format}"  $[bitrate / 1000] $minutes $seconds \
-                > "$datafile"
+                > "${reply[2]}"
             )&
-            ffmpegthumbnailer -s 512 -m -i "$FILE" -o "$tmpfile"&
+            ffmpegthumbnailer -s 512 -m -i "$FILE" -o "${reply[1]}"&
             wait
-        }
-        cat "$datafile"
-        display_image "$tmpfile" 8
-        exit 1
+        fi
+        cat "${reply[2]}"
+        display_image "${reply[1]}" 8
         ;;
 
 
     *x-iso9660-image)
-        datafile="$(create_cache "${FILE}" ".desc")"
-        if [[ ! -f "$datafile" ]]; then
+        create_cache "${FILE}" ".desc"
+        if create_cache "${FILE}" ".desc"; then
             local keywords=1 application creator publisher volume
             local -a files
             iso-info --no-header -i "$FILE"|while read -r line; do
@@ -147,52 +152,44 @@ case "$MIMETYPE" in
                     files+=("$file")
                 fi
             done
-            print "Name: ${application:--}\nCreated by: ${creator:--}\nPublisher: ${publisher:--}\nVolume: ${volume:--}\n\nFile Listing:" > "$datafile"
-            print "${(j:\n:)files}" >> "$datafile"
+            print "Name: ${application:--}\nCreated by: ${creator:--}\nPublisher: ${publisher:--}\nVolume: ${volume:--}\n\nFile Listing:" > "${reply[1]}"
+            print "${(j:\n:)files}" >> "${reply[1]}"
         fi
-        cat "$datafile"
-        exit 1
+        cat "${reply[1]}"
         ;;
 
     *opendocument*|application/vnd.openxmlformats-officedocument.*)
-        tmpfile="$(create_cache "$FILE" ".png")"
-        if ! [[ -f "$tmpfile" ]] {
+        if create_cache "$FILE" ".png"; then
             libreoffice --convert-to pdf "$FILE" --outdir "$CACHEDIR" >/dev/null 2>&1
             outfile="$CACHEDIR/${FILE:t}" 2>/dev/null
             outfile="${outfile%.*}.pdf" 2>/dev/null
-            pdftoppm -f 1 -l 1 -png "$outfile" >> "$tmpfile" 2>/dev/null
+            pdftoppm -f 1 -l 1 -png "$outfile" >> "${reply[1]}" 2>/dev/null
             rm "$outfile"
-        }
-        display_image "$tmpfile"
-        exit 1
+        fi
+        display_image "${reply[1]}"
         ;;
 
     text/*|*/xml|application/javascript|application/pgp-signature|application/x-setupscript|application/x-wine-extension-ini)
         COLORTERM=truecolor bat -pf --wrap=character --terminal-width=$[W-4] -f --number \
             --line-range 1:$[LINES - 2] "$FILE"
-        exit 1
         ;;
 
     application/json)
         jq -C < "$FILE"
-        exit 1
         ;;
 
     *octet-stream)
         print -P "%SBinary\e[0m"
         xxd -a -R always -c $[(COLUMNS / 6) + 1] -u -l $[(( (COLUMNS / 6) + 1) * LINES) + 256 ] "$FILE"
-        exit 1
         ;;
 
     inode/x-empty|application/x-empty)
         print -P "%SEmpty\e[0m"
-        exit 1
         ;;
 
     application/x-archive|application/x-cpio|application/x-tar|application/x-bzip2|application/gzip|application/x-lzip|application/x-lzma|application/x-xz|application/x-7z-compressed|application/vnd.android.package-archive|application/vnd.debian.binary-package|application/java-archive|application/x-gtar|application/zip)
         info "󰛫 ${MIMETYPE//application\//} Archive"
         bsdtar --list --file "$FILE"
-        exit 1
         ;;
     application/x-rar-compressed|application/x-rar)
         info "󰛫 rar Archive"
@@ -205,7 +202,6 @@ case "$MIMETYPE" in
                 flag=0
             }
             done
-        exit 1
         ;;  
     application/x-pie-executable|application/x-executable|application/x-sharedlib)
         case "$MIMETYPE" in 
@@ -233,7 +229,6 @@ case "$MIMETYPE" in
         print "\nLinked against:
 ${(j:
 :)libs[@]}"
-        exit 1
         ;;
     application/x-object)
         info "󰈮 Object File"
@@ -257,7 +252,6 @@ ${(j:, :)vars}
 $#undef Undefined Symbol(s):
 ${(j:, :)undef}"|fmt -sw $((W-4))
 
-        exit 1
         ;;
 
     application/vnd.flatpak.ref)
@@ -280,7 +274,6 @@ ${(j:, :)undef}"|fmt -sw $((W-4))
             esac
         done < "$FILE"
         print "$name: $desc\nVersion: $ver\nHomepage: $url\nRepo: $repo"
-        exit 1
         ;;
 
     font/sfnt|application/vnd.ms-opentype)
@@ -289,27 +282,22 @@ abcdefghijklmnopqrstuvwxyz
 01234567890 $€£¥
 () {} [] <> \\\|/ +-*= ¿?¡! .,:; &%#@^
 The quick brown fox jumps over the lazy dog.
-Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich.
-"
-        tmpfile="$(create_cache "${FILE}" ".png")"
-        datafile="$(create_cache "${FILE}" ".desc")"
-        if ! [[ -f "$tmpfile" ]] {
-            convert -background transparent -fill '#eceff4' -font "$FILE" \
-                -pointsize 12 label:"$example_text" \
-                "$tmpfile" &
+Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich."
+        if create_cache "${FILE}" .png .desc; then
+            magick -background transparent -fill '#eceff4' -font "$FILE" \
+                -pointsize 24 label:"$example_text" \
+                "${reply[1]}" &
             fc-scan \
                 --format="Name: %{fullname}\nFamily: %{family}\nPostscript: %{postscriptname}\nStyle(s): %{style}\n" \
-                -- "$FILE" > "$datafile"&
+                -- "$FILE" > "${reply[2]}"&
             wait
-        }
-        cat "$datafile"
-        display_image "$tmpfile" 5
-        exit 1
+        fi
+        cat "${reply[2]}"
+        display_image "${reply[1]}" 5
         ;;
 
     *)
         info "󰈔 ${MIMETYPE}"
-        exit 1
         ;;
 esac
-
+exit 1
