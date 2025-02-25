c8b3baec2d052db537791dd633309cf11a2e2fd8
dmxk062 <mxk06@protonmail.com>
2025-02-25 17:06:59 +0100

    sway: add bind to stop and resume window's process

---
+++ .config/sway/scripts/stop_continue_window.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+pid="$(swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused).pid')"
+if [ "$(ps -o state= $pid)" = T ]; then
+    kill -CONT $pid
+else
+    kill -STOP $pid
+fi
