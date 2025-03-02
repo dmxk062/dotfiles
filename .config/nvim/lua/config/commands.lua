01860e5d9caa3f4fee71cb2320ad3846cf68ea75
dmxk062 <mxk06@protonmail.com>
2025-03-02 11:53:04 +0100

    nvim: add Sp[lit] as a smarter split command

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,3 +1,17 @@
+---@class vim.user_command_args
+---@field name string
+---@field args string
+---@field fargs string[]
+---@field nargs string
+---@field bang boolean
+---@field line1 number
+---@field line2 number
+---@field range 0|1|2
+---@field count number
+---@field reg string
+---@field mods string
+---@field smods table
+
 local function get_zoxide_result(path)
     local expanded = path:gsub("~", vim.env.HOME)
     local cmd = { "zoxide", "query", expanded}
@@ -32,3 +46,34 @@ end, {
     complete = complete_zoxide,
     desc = "Use zoxide to open dir in an oil buffer"
 })
+
+---@param args vim.user_command_args
+local function smart_split(args)
+    local split_args = {}
+
+    if args.args ~= "" then
+        split_args[1] = args.args
+    end
+
+    local height = vim.api.nvim_win_get_height(0)
+    local width = vim.api.nvim_win_get_width(0)
+
+    local cmd
+    if height > width / 1.61 then
+        cmd = "split"
+    else
+        cmd = "vsplit"
+    end
+
+    vim.cmd[cmd](split_args)
+end
+
+
+local split_cmd_opts = {
+    complete = "file",
+    nargs = "?",
+    count = 0,
+}
+
+vim.api.nvim_create_user_command("Sp", smart_split, split_cmd_opts)
+vim.api.nvim_create_user_command("Split", smart_split, split_cmd_opts)
