ee7f2ac69556ecc113ca210eba3651bf60467ee6
dmxk062 <mxk06@protonmail.com>
2025-03-06 17:47:19 +0100

    nvim: use exchange operator

--- .config/nvim/lua/plugins/substitute.lua
+++ .config/nvim/lua/plugins/substitute.lua
@@ -16,6 +16,21 @@ local M = {
         map("n", "gss", substitute.line)
         map("n", "gS", substitute.eol)
         map("x", "gs", substitute.visual)
+
+        -- i dont really want to override gx, but it makes more sense this way
+        local exchange = require("substitute.exchange")
+        map("n", "gx", exchange.operator)
+        map("n", "gxx", exchange.line)
+        map("x", "gx", exchange.visual)
+
+        map("n", "go", function()
+            vim.ui.open(vim.fn.expand("<cfile>"))
+        end)
+
+        map("x", "go", function()
+            local lines = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), { type = vim.fn.mode() })
+            vim.ui.open(table.concat(vim.iter(lines):map(vim.trim):totable()))
+        end)
     end
 }
 
