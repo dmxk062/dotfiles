00196d7ba7f0cde6d339eb1358a1b570595e34b1
dmxk062 <mxk06@protonmail.com>
2025-03-08 13:04:50 +0100

    zsh/scripts: move translate script into scripts

--- .config/zsh/mods/net.zsh
+++ .config/zsh/mods/net.zsh
@@ -6,7 +6,6 @@ if [[ "$1" == "unload" ]]; then
     unfunction fupload \
         urlenc urldec \
         makeqr \
-        deepl_request translate \
         ncsend ncrecv ncsenddir ncrecvdir
 
     unalias req 
@@ -59,41 +58,9 @@ function urldec {
     done
 }
 
-function deepl_request {
-    local request="$1"
-    local auth_key="$( < "$XDG_DATA_HOME/keys/deepl" )"
-    if [[ "$auth_key" == "" ]]; then
-        print -P -- "%F{red}Please make sure your auth key is placed at $XDG_DATA_HOME/keys/deepl%f" > /dev/stderr
-        return 1
-    fi
-    curl -s --request POST --header "Authorization: DeepL-Auth-Key $auth_key" \
-        --header "Content-Type: application/json" \
-        --data "$request" \
-        'https://api-free.deepl.com/v2/translate'
-}
-
 function makeqr {
     qrencode --size=2 --margin=2 --type=ANSIUTF8 --output=- 
 }
-
-function translate {
-    local target="${1:-EN}"
-    local -a buffer
-    local line
-    while read -r line; do
-        buffer+=("$line")
-    done
-    local request_body
-    if [[ -n "$2" ]]; then
-        request_body="{\"text\":[\"${(j:\n:)buffer[@]}\"],\"target_lang\":\"$target\", \"source_lang\": \"$2\"}"
-    else
-        request_body="{\"text\":[\"${(j:\n:)buffer[@]}\"],\"target_lang\":\"$target\"}"
-    fi
-    deepl_request "$request_body"|jq '.translations.[0].text' -r
-}
-
-autoload -Uz _translate
-compdef _translate translate
 
 function ncsend {
     cat | nc "$1" $NC_PORT
