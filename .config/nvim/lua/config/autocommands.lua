687b394b451b9c934ffe3c20b0145ae7d0dd95fd
dmxk062 <mxk06@protonmail.com>
2025-03-15 11:30:53 +0100

    nvim: saner autoclose for terminal

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,11 +1,12 @@
 local autocmd = vim.api.nvim_create_autocmd
+local api = vim.api
 local utils = require("config.utils")
 
 -- Window Title {{{
 -- change the title in a more intelligent way
 autocmd({ "BufEnter", "BufReadPost", "BufNewFile", "VimEnter" }, {
     callback = function(args)
-        local name, kind, show_modified = require("config.utils").format_buf_name(vim.api.nvim_get_current_buf(), true)
+        local name, kind, show_modified = require("config.utils").format_buf_name(api.nvim_get_current_buf(), true)
 
         vim.o.titlestring = "nv: " .. (name or "[-]")
     end
@@ -18,7 +19,7 @@ change line number based on mode:
 - command mode: make it absolute for ranges etc
 - normal mode: keep relative motions fast
 ]]
-local cmdline_group = vim.api.nvim_create_augroup("CmdlineLinenr", {})
+local cmdline_group = api.nvim_create_augroup("CmdlineLinenr", {})
 -- debounce cmdline enter events to make sure we dont have flickering for non user cmdline use
 -- e.g. mappings using : instead of <cmd>
 local cmdline_debounce_timer
@@ -30,7 +31,7 @@ autocmd("CmdlineEnter", {
         cmdline_debounce_timer:start(100, 0, vim.schedule_wrap(function()
             if vim.o.number then
                 vim.o.relativenumber = false
-                vim.api.nvim__redraw({ statuscolumn = true })
+                api.nvim__redraw({ statuscolumn = true })
             end
         end))
     end
@@ -81,6 +82,22 @@ autocmd({ "BufWinEnter", "WinEnter" }, {
         end
     end
 })
+
+-- automatically close interactive terminal buffers
+local nvim_builtin_termclose = api.nvim_get_autocmds { group = "nvim_terminal", event = "TermClose" }
+api.nvim_del_autocmd(nvim_builtin_termclose[1].id)
+autocmd("TermClose", {
+    nested = true, -- trigger BufDelete
+    callback = function(ev)
+        if vim.v.event.status ~= 0 then
+            return
+        end
+
+        if vim.b[ev.buf].term_autoclose then
+            api.nvim_buf_delete(ev.buf, { force = true })
+        end
+    end
+})
 -- }}}
 
 -- Smarter :h 'autocd' {{{
@@ -93,7 +110,7 @@ autocmd("BufReadPost", {
             return
         end
 
-        local path = vim.api.nvim_buf_get_name(ev.buf)
+        local path = api.nvim_buf_get_name(ev.buf)
         local git_root = vim.fs.root(path, ".git")
         local pwd = vim.fn.getcwd()
         if git_root and not vim.startswith(pwd, git_root) then
@@ -123,7 +140,7 @@ autocmd("TextYankPost", {
 -- show current directory
 local function printdir()
     local name = utils.expand_home(vim.fn.getcwd(0, 0))
-    vim.api.nvim_echo({ { "pwd: ", "NonText" }, { name, "Directory" } }, false, {})
+    api.nvim_echo({ { "pwd: ", "NonText" }, { name, "Directory" } }, false, {})
 end
 
 autocmd("DirChanged", {
