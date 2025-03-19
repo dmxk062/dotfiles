e788c8448262348c52708889fdbc0c25a47f68f0
dmxk062 <mxk06@protonmail.com>
2025-03-19 12:01:01 +0100

    rofi: keep selection in wallpapers

--- .config/rofi/applets/wallpapers/wallpapers.sh
+++ .config/rofi/applets/wallpapers/wallpapers.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-PROMPT="\0prompt\x1f"
 ICON="\0icon\x1f"
 SET_DELIM="\0delim\x1f"
 
@@ -13,8 +12,10 @@ print_walls() {
 
 if ((ROFI_RETV != 0)); then
     ~/.config/background/wallpaper.sh both "$XDG_CONFIG_HOME/background/img/$1" >/dev/null 2>&1
+    echo -en "$SETSEL$ROFI_INFO"
 else
     echo -en "$SET_DELIM\t\n"
 fi
+echo -en "\0keep-selection\x1ftrue\t"
 
 print_walls
