1f8b6fe7d9d9437bbb7084fbffb1b251426300d6
dmxk062 <mxk06@protonmail.com>
2025-03-19 10:06:49 +0100

    nvim: improve lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,3 +1,4 @@
+-- Spec {{{
 local M = {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -20,6 +21,7 @@ local M = {
                 }
 
                 require("mason-lspconfig").setup {
+                    automatic_installation = true,
                     ensure_installed = {
                         "ruff",
                         "asm_lsp",
@@ -35,9 +37,10 @@ local M = {
     },
     event = { "BufReadPost", "BufNewFile" }
 }
+-- }}}
 
+-- Keymapping {{{
 local utils = require("config.utils")
-
 local lsp_map = function(buf)
     local map = utils.local_mapper(buf, { group = true })
     map({ "n", "v" }, "<space>a", vim.lsp.buf.code_action)
@@ -64,82 +67,166 @@ local lsp_map = function(buf)
         })
     end)
     map("n", "<space>c", "<space>rc", { remap = true })
+
+    -- keep the classic rename around, useful for symbols that aren't exactly a <cword>
     map("n", "<space>R", vim.lsp.buf.rename)
 
-    map("n", "<space>Ql", function()
-        vim.fn.setqflist(vim.diagnostic.toqflist(vim.diagnostic.get(0)))
-        require("quicker").open()
-    end)
-    map("n", "<space>Ll", function()
-        vim.fn.setloclist(0, vim.diagnostic.toqflist(vim.diagnostic.get(0)))
-        require("quicker").open { loclist = true }
-    end)
+    -- list lsp things and use telescope to disambiguate
+    map("n", "gd", function() require("telescope.builtin").lsp_definitions() end)
+    map("n", "gr", function() require("telescope.builtin").lsp_references() end)
+    map("n", "gi", function() require("telescope.builtin").lsp_implementations() end)
 
-
-    map("n", "gr", require("telescope.builtin").lsp_references)
-    map("n", "gd", require("telescope.builtin").lsp_definitions)
+    -- go to the thing in a split and use the qflist to disambiguate instead of telescope
     map("n", "<C-w>gd", function()
         utils.open_window_smart(0, { enter = true })
-        vim.lsp.buf.definition { reuse_win = false }
+        vim.lsp.buf.definition { reuse_win = false, loclist = true }
         vim.cmd.normal("zz")
     end)
     map("n", "<C-w>gr", function()
         utils.open_window_smart(0, { enter = true })
-        vim.lsp.buf.definition { reuse_win = false }
+        vim.lsp.buf.references(nil, { loclist = true })
         vim.cmd.normal("zz")
     end)
-    map("n", "gi", require("telescope.builtin").lsp_implementations)
+    map("n", "<C-w>gi", function()
+        utils.open_window_smart(0, { enter = true })
+        vim.lsp.buf.implementation { reuse_win = false, loclist = true }
+        vim.cmd.normal("zz")
+    end)
 end
 
+local on_attach = function(ev)
+    lsp_map(ev.buf)
+    vim.api.nvim_buf_create_user_command(ev.buf, "InlayHint", function(args)
+        local cmd = args.fargs[1]
+        if cmd then
+            if cmd == "on" then
+                vim.lsp.inlay_hint.enable(true)
+            elseif cmd == "off" then
+                vim.lsp.inlay_hint.enable(false)
+            end
+        else
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        end
+    end, {
+        nargs = "?",
+        complete = function()
+            return { "on", "off" }
+        end
+    })
+end
+
+local on_detach = function(ev)
+    utils.unmap_group(ev.buf)
+    vim.api.nvim_buf_del_user_command(ev.buf, "InlayHint")
+end
+-- }}}
+
+-- Configs {{{
+local L = {}
+
+-- wrap in a function to delay loading schemastore
+L.jsonls = function()
+    return {
+        capabilities = true,
+        settings = {
+            json = {
+                schemas = require("schemastore").json.schemas(),
+                validate = { enable = true },
+            },
+        },
+    }
+end
+
+L.lua_ls = {
+    capabilities = true,
+    settings = {
+        Lua = {}
+    }
+}
+L.lua_ls.on_init = function(client)
+    local path = client.workspace_folders[1].name
+
+    local is_in_rtp = false
+    for _, elem in pairs(vim.opt.runtimepath:get()) do
+        if vim.startswith(path, elem) then
+            is_in_rtp = true
+            break
+        end
+    end
+    if not is_in_rtp and not vim.startswith(path, vim.fn.stdpath("data")) then
+        return
+    end
+
+    local libpaths = {
+        vim.env.VIMRUNTIME,                 -- runtime files
+        vim.fn.stdpath("config") .. "/lua", -- config files
+        "${3rd}/luv/library",               -- vim.uv
+    }
+
+    -- load lazy plugins for those that do use lua
+    for _, plug in pairs(require("lazy").plugins()) do
+        local dir = plug.dir .. "/lua"
+        if vim.uv.fs_stat(dir) then
+            table.insert(libpaths, dir)
+        end
+    end
+
+    -- load nvim-specific libraries only for config
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+        runtime = {
+            version = "LuaJIT"
+        },
+        workspace = {
+            checkThirdParty = false,
+            library = libpaths,
+        }
+    })
+end
+
+L.clangd = {
+    capabilities = true,
+    cmd = {
+        "clangd", "--enable-config", "--background-index"
+    },
+    on_attach = function()
+        local map = utils.local_mapper(vim.api.nvim_get_current_buf(), { group = true })
+        map("n", "gH", "<cmd>ClangdSwitchSourceHeader<CR>")
+    end
+}
+
+L.asm_lsp = {
+    capabilities = true,
+    root_dir = function(path)
+        return vim.fs.root(path, ".git")
+    end
+}
+
+L.marksman = {} -- crashes with my capabilities for some reasons
+
+L.hls = {
+    capabilities = true,
+    filetypes = { 'haskell', 'lhaskell', 'cabal' },
+}
+
+for _, lsp in pairs({ "bashls", "ts_ls", "html", "jedi_language_server", "ruff", "taplo", "yamlls" }) do
+    L[lsp] = { capabilities = true }
+end
+-- }}}
 
 M.config = function()
     local lspconfig = require("lspconfig")
 
-    require("lspconfig.ui.windows").default_options = {
-        border = "rounded"
-    }
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     capabilities.textDocument.foldingRange = {
         dynamicRegistration = false,
         lineFoldingOnly = true
     }
 
-    local augroup = vim.api.nvim_create_augroup("UserLspConfig", {})
-    vim.api.nvim_create_autocmd("LspAttach", {
-        group = augroup,
-        callback = function(opts)
-            lsp_map(opts.buf)
-            vim.api.nvim_buf_create_user_command(opts.buf, "InlayHint", function(args)
-                local cmd = args.fargs[1]
-                if cmd then
-                    if cmd == "on" then
-                        vim.lsp.inlay_hint.enable(true)
-                    elseif cmd == "off" then
-                        vim.lsp.inlay_hint.enable(false)
-                    end
-                else
-                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-                end
-            end, {
-                nargs = "?",
-                complete = function()
-                    return { "on", "off" }
-                end
-            })
-        end,
-    })
-
-    vim.api.nvim_create_autocmd("LspDetach", {
-        group = augroup,
-        callback = function(ev)
-            utils.unmap_group(ev.buf)
-
-            vim.api.nvim_buf_del_user_command(ev.buf, "InlayHint")
-        end
-    })
-
+    -- rounded corners for everything
+    require("lspconfig.ui.windows").default_options = {
+        border = "rounded"
+    }
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
         vim.lsp.handlers.hover, {
             border = "rounded",
@@ -150,7 +237,26 @@ M.config = function()
             border = "rounded"
         }
     )
+    vim.diagnostic.config {
+        float = {
+            border = "rounded",
+        },
+        virtual_text = {
+            prefix = "!",
+        }
+    }
 
+    local augroup = vim.api.nvim_create_augroup("UserLspConfig", {})
+    vim.api.nvim_create_autocmd("LspAttach", {
+        group = augroup,
+        callback = on_attach,
+    })
+    vim.api.nvim_create_autocmd("LspDetach", {
+        group = augroup,
+        callback = on_detach,
+    })
+
+    -- remove sign text
     local signs = {
         "DiagnosticSignError",
         "DiagnosticSignWarn",
@@ -161,83 +267,15 @@ M.config = function()
         vim.fn.sign_define(sign, { texthl = sign, text = "", numhl = sign })
     end
 
-    vim.diagnostic.config {
-        float = {
-            border = "rounded",
-        },
-        virtual_text = {
-            prefix = "!",
-        }
-    }
-
-    -- individual lsps
-    lspconfig.jsonls.setup {
-        capabilities = capabilities,
-        settings = {
-            json = {
-                schemas = require("schemastore").json.schemas(),
-                validate = { enable = true },
-            },
-        },
-    }
-    lspconfig.lua_ls.setup {
-        capabilities = capabilities,
-        on_init = function(client)
-            local path = client.workspace_folders[1].name
-            -- we're editing some other lua project, not the nvim config
-            if not (vim.startswith(path, vim.fn.stdpath("config")) or vim.startswith(path, vim.fn.stdpath("data"))) then
-                return
-            end
-            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                runtime = {
-                    version = "LuaJIT"
-                },
-                workspace = {
-                    checkThirdParty = false,
-                    -- library = vim.api.nvim_get_runtime_file("", true), -- slow, has **everything**
-                    library = {
-                        -- dont get everything, only the stuff i truly care about
-                        vim.env.VIMRUNTIME,
-                        vim.fn.stdpath("config") .. "/lua"
-                    }
-                }
-            })
-        end,
-        settings = {
-            Lua = {}
-        }
-    }
-    lspconfig.clangd.setup {
-        capabilities = capabilities,
-        cmd = {
-            "clangd", "--enable-config", "--background-index"
-        },
-        on_attach = function()
-            local map = utils.local_mapper(vim.api.nvim_get_current_buf(), { group = true })
-            map("n", "gH", "<cmd>ClangdSwitchSourceHeader<CR>")
+    for lsp, config in pairs(L) do
+        if type(config) == "function" then
+            config = config()
         end
-    }
-    lspconfig.asm_lsp.setup {
-        capabilities = capabilities,
-        root_dir = function(path)
-            return vim.fs.root(path, ".git")
+
+        if config.capabilities == true then
+            config.capabilities = capabilities
         end
-    }
-
-    -- for some reason it likes to crash with the capabilities
-    lspconfig.marksman.setup {
-
-    }
-
-    lspconfig.hls.setup {
-        capabilities = capabilities,
-        filetypes = { 'haskell', 'lhaskell', 'cabal' },
-    }
-    -- dont need anything special from those *yet*
-    for _, lsp in pairs({ "bashls", "ts_ls", "html", "jedi_language_server", "ruff", "taplo", "yamlls", }) do
-        lspconfig[lsp].setup {
-            capabilities = capabilities
-        }
+        lspconfig[lsp].setup(config)
     end
 end
 
