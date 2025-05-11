53029a8dd39931e0e47748950b73caa310812cb5
dmxk062 <mxk06@protonmail.com>
2025-05-11 18:10:26 +0200

    nvim: significantly increase oil git status usefulness

--- .config/nvim/lua/config/plugins/oil_git.lua
+++ .config/nvim/lua/config/plugins/oil_git.lua
@@ -2,7 +2,7 @@ local M = {}
 
 --[[
 Show git status of a file as virtual text
-]]--
+]] --
 
 local api = vim.api
 local oil = require("oil")
@@ -24,6 +24,7 @@ local hl_for_status = {
 }
 
 local function set_signs(buf, status)
+    buffer_status[buf] = status
     api.nvim_buf_clear_namespace(buf, ns, 0, -1)
 
     if not status then
@@ -56,7 +57,7 @@ local function set_signs(buf, status)
     end
 end
 
-local function async_at_once(funcs, cb)
+local function async_at_once(funcs, cb, ...)
     local count = 0
     local results = {}
     for i, fn in ipairs(funcs) do
@@ -67,8 +68,77 @@ local function async_at_once(funcs, cb)
             if count == #funcs then
                 cb(results)
             end
-        end)
+        end, ...)
     end
+end
+
+local function get_git_status(cb, dir)
+    vim.system(
+        { "git", "-c", "core.quotepath=false", "-c", "status.relativePaths=true", "status", ".", "--short" },
+        { cwd = dir, },
+        function(out)
+            if out.code ~= 0 then
+                return
+            end
+
+            local status = {}
+            for line in vim.gsplit(out.stdout, "\n") do
+                if line == "" then
+                    goto continue
+                end
+                if line:sub(-1, -1) == "/" then
+                    line = line:sub(1, -2)
+                end
+
+                local index = line:sub(1, 1)
+                local worktree = line:sub(2, 2)
+                local file = line:sub(4)
+
+                local dirstart = file:find("/")
+
+                if dirstart then
+                    file = file:sub(1, dirstart - 1)
+                    if not status[file] then
+                        status[file] = { index == "?" and " " or index, worktree }
+                    else
+                        if index ~= " " and index ~= "?" then
+                            status[file][1] = "M"
+                        end
+                        if worktree ~= " " then
+                            status[file][2] = "M"
+                        end
+                    end
+                else
+                    status[file] = { index, worktree }
+                end
+
+                ::continue::
+            end
+            cb(status)
+        end)
+end
+
+local function get_git_unchanged(cb, dir)
+    vim.system(
+        { "git", "-c", "core.quotepath=false", "ls-tree", "HEAD", ".", "--name-only" },
+        { cwd = dir },
+        function(out)
+            if out.code ~= 0 then
+                return
+            end
+
+            local status = {}
+            for line in vim.gsplit(out.stdout, "\n") do
+                if line == "" then
+                    goto continue
+                end
+
+                status[line] = { " ", " " }
+                ::continue::
+            end
+
+            cb(status)
+        end)
 end
 
 local function update_buf(buf)
@@ -77,65 +147,16 @@ local function update_buf(buf)
         return
     end
 
-
     async_at_once({
-            function(cb)
-                vim.system(
-                    { "git", "-c", "core.quotepath=false", "-c", "status.relativePaths=true", "status", ".", "--short" },
-                    { cwd = dir, },
-                    function(out)
-                        if out.code ~= 0 then
-                            return
-                        end
-
-                        local status = {}
-                        for line in vim.gsplit(out.stdout, "\n") do
-                            if line == "" then
-                                goto continue
-                            end
-                            local entry, slashcount = line:gsub("/", "")
-                            if (line:sub(-1, -1) ~= "/" and slashcount ~= 0) or slashcount > 1 then
-                                goto continue
-                            end
-
-                            local index = entry:sub(1, 1)
-                            local worktree = entry:sub(2, 2)
-                            local file = entry:sub(4)
-
-                            status[file] = { index, worktree }
-
-                            ::continue::
-                        end
-                        cb(status)
-                    end)
-            end,
-            function(cb)
-                vim.system(
-                    { "git", "-c", "core.quotepath=false", "ls-tree", "HEAD", ".", "--name-only" },
-                    { cwd = dir },
-                    function(out)
-                        if out.code ~= 0 then
-                            return
-                        end
-
-                        local status = {}
-                        for line in vim.gsplit(out.stdout, "\n") do
-                            if line == "" then
-                                goto continue
-                            end
-
-                            status[line] = { " ", " " }
-                            ::continue::
-                        end
-
-                        cb(status)
-                    end)
-            end,
+            get_git_status,
+            get_git_unchanged,
         },
         vim.schedule_wrap(function(results)
             local merged = vim.tbl_extend("keep", results[1], results[2])
             set_signs(buf, merged)
-        end))
+        end),
+        dir
+    )
 end
 
 M.attach = function(buf)
@@ -160,6 +181,16 @@ M.attach = function(buf)
         group = group,
         callback = function()
             update_buf(buf)
+        end
+    })
+
+    api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+        buffer = buf,
+        group = group,
+        callback = function()
+            if buffer_status[buf] and buffer_status[buf] ~= true then
+                set_signs(buf, buffer_status[buf])
+            end
         end
     })
 
