410bb758a5daa76e563fc22aee58046b432ee9ae
dmxk062 <mxk06@protonmail.com>
2025-05-29 10:33:31 +0200

    sway: improve recording script

--- .config/sway/scripts/record.sh
+++ .config/sway/scripts/record.sh
@@ -1,51 +1,50 @@
 #!/usr/bin/env bash
 
-DATEFMT="%Y_%m.%d_%H:%M:%S_sc"
+DATEFMT="%Y_%m.%d_%H:%M:%S"
 VIDEO_DIR="$(xdg-user-dir "VIDEOS")/Recordings"
 
 function create_file {
     timestamp="$(date +"$DATEFMT")"
-    REPLY="$VIDEO_DIR/${timestamp}_rec.mkv"
+    REPLY="$VIDEO_DIR/${timestamp}.mkv"
 }
 
-if [[ "$1" == "start" ]]; then
-    if pkill wf-recorder; then
-        exit
-    fi
-    screen="$(swaymsg -t get_outputs | jq -r '.[]|select(.focused).name')"
-    if [[ "$2" == "select" ]]; then
-        region="$(slurp -w 0 -b '#4c566acc' -s '#ffffff00')"
-        if [[ -z "$region" ]]; then
-            exit
-        fi
-        extra="-g"
-        sleep 0.1
-    fi
-
-    create_file
-    path="$REPLY"
-    echo "$region"
-    wf-recorder -o $screen  $extra "$region" -c hevc_vaapi -d /dev/dri/renderD128 -f "$path" -r 60 &
-    pid=$!
-    printf -v struct '{"start":%s, "pid":%s, "path":"%s"}' $EPOCHSECONDS $pid "$path"
-    eww -c "$XDG_CONFIG_HOME/eww/shell/" update recording=true recording-info="$struct"
-
-    wait $pid
-    eww -c "$XDG_CONFIG_HOME/eww/shell/" update recording=false recording-info='{}'
-
-    CACHEFILE="$XDG_CACHE_HOME/.thumb_$EPOCHSECONDS"
-    ffmpegthumbnailer -m -i "$path" -o "$CACHEFILE"
-    reply="$(notify-send "Finished recording video" "$path"\
-        -i "$CACHEFILE" \
-        --action=open="Open"\
-        --action=del="Delete"\
-        --action=copy="Copy"\
-    )"
-    unlink "$CACHEFILE"
-    case "$reply" in 
-        open) xdg-open "$path";;
-        del) rm "$path";;
-        copy) wl-copy < "$path";;
-    esac
+if pkill wf-recorder; then
+    exit
 fi
 
+screen="$(swaymsg -t get_outputs | jq -r '.[]|select(.focused).name')"
+if [[ "$2" == "select" ]]; then
+    region="$(slurp -w 0 -b '#4c566acc' -s '#ffffff00')"
+    if [[ -z "$region" ]]; then
+        exit
+    fi
+    extra="-g"
+    sleep 0.1
+fi
+
+create_file
+path="$REPLY"
+
+wf-recorder -o "$screen" $extra "$region" -c hevc_vaapi -d /dev/dri/renderD128 -f "$path" -r 60 &
+pid=$!
+printf -v info '{"start":%s, "pid":%s, "path":"%s"}' $EPOCHSECONDS $pid "$path"
+eww -c "$XDG_CONFIG_HOME/eww/shell/" update recording=true recording-info="$info"
+
+wait $pid
+eww -c "$XDG_CONFIG_HOME/eww/shell/" update recording=false recording-info='{}'
+
+CACHEFILE="$XDG_CACHE_HOME/.thumb_$EPOCHSECONDS"
+ffmpegthumbnailer -m -i "$path" -o "$CACHEFILE"
+reply="$(
+    notify-send "Finished recording video" "$path" \
+        -i "$CACHEFILE" \
+        --action=open="Open" \
+        --action=del="Delete" \
+        --action=copy="Copy"
+)"
+unlink "$CACHEFILE"
+case "$reply" in
+open) xdg-open "$path" ;;
+del) rm "$path" ;;
+copy) wl-copy -t text/uri-list "file://$path" ;;
+esac
