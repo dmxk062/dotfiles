4338099dd8e1d2a6e4ea33ce6a2cd8dfa4caed16
dmxk062 <mxk06@protonmail.com>
2025-06-01 10:41:18 +0200

    eww: add timer to the desktop clock

---
+++ .config/eww/shell/bin/set_timer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+response="$(zenity --entry --text="[HH:]MM:SS" --title="Create Timer" --entry-text "5:00")"
+IFS=: read -ra fields <<<"$response"
+
+time=0
+num_fields=${#fields[@]}
+if ((num_fields == 1)); then
+    time=${fields[0]}
+elif ((num_fields == 2)); then
+    time=$((fields[0] * 60 + fields[1]))
+else
+    time=$((fields[0] * 3600 + fields[1] * 60 + fields[2]))
+fi
+
+if ((time <= 0)); then
+    exit
+fi
+
+eww -c "$XDG_CONFIG_HOME/eww/shell" update timer-start=$EPOCHSECONDS timer-time=$((EPOCHSECONDS + time))
+
+sleep "$time"
+notify-send "Timer Done"
+eww -c "$XDG_CONFIG_HOME/eww/shell" update timer-start=0 timer-time=0
