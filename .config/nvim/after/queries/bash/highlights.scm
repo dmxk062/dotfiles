ee53c0d5983d56a590a2a024645e2539516a6081
dmxk062 <mxk06@protonmail.com>
2025-06-17 15:21:42 +0200

    nvim: highlight associative arrays for bash

--- .config/nvim/after/queries/bash/highlights.scm
+++ .config/nvim/after/queries/bash/highlights.scm
@@ -1,9 +1,20 @@
 ; extends
 
 ; usually "--" as a command argument is more of a metasyntactic thing
-(command 
-  argument: [
-             (word) @punctuation.delimiter
-
-             ]
+(command
+  argument: [ (word) @punctuation.delimiter ]
   (#eq? @punctuation.delimiter "--"))
+
+; Highlight associative array declarations
+; TODO: highlight simple forms, e.g.
+; declare -A array=(
+;   [val]=1
+; )
+(array
+  (concatenation 
+    (word) @punctuation.delimiter
+    [ (string) (number) ((word) @property) ]
+    (word) @punctuation.delimiter
+    (word) @operator
+  (#eq? @operator "=")
+  (#any-of? @punctuation.delimiter "]" "[")))
