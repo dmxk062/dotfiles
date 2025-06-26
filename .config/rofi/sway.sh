6ec4aeef8d138d74b0e7323f58164c1158933f8d
dmxk062 <mxk06@protonmail.com>
2025-06-26 09:33:54 +0200

    rofi: also show floating windows in window list

--- .config/rofi/sway.sh
+++ .config/rofi/sway.sh
@@ -3,6 +3,13 @@
 ICON="\0icon\x1f"
 INFO="\x1finfo\x1f"
 
+read -r -d '' JQ_QUERY << 'JQ'
+.. | ((.nodes? // empty)
+    + (.floating_nodes? // empty))[] 
+| select(.pid)
+| "\(.id)\t\(.name)\t\(.app_id? // .window_properties.class)\t\(if .focused then 1 else 0 end)"
+JQ
+
 if ((ROFI_RETV == 0)); then
     echo -en "\0use-hot-keys\x1ftrue\n\0delim\x1f\t\n"
     while IFS=$'\t' read -r id name class is_active; do
@@ -19,8 +26,7 @@ if ((ROFI_RETV == 0)); then
             esac
         fi
         printf "%s\n%s$ICON%s$INFO%s\t" "$name" "$class" "$icon" "$id"
-    done < <(swaymsg -t get_tree |
-        jq -r '.. | (.nodes? // empty)[] | select(.pid) | "\(.id)\t\(.name)\t\(.app_id? // .window_properties.class)\t\(if .focused then 1 else 0 end)"')
+    done < <(swaymsg -t get_tree | jq -r "$JQ_QUERY")
 else
     if ((ROFI_RETV == 10)); then
         swaymsg "[con_id=$ROFI_INFO] move to workspace current" > /dev/null
