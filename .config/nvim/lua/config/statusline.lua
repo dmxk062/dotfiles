9ebf9f7e10bb756908ecd208f861357ce6b48e30
dmxk062 <mxk06@protonmail.com>
2025-06-28 11:00:13 +0200

    nvim: update statusline

--- .config/nvim/lua/config/statusline.lua
+++ .config/nvim/lua/config/statusline.lua
@@ -14,56 +14,39 @@ local function esc(str)
     return str:gsub("%%", "%%%%")
 end
 
-local function padd(str, len)
-    local strlen = #str
-    if strlen >= len then
-        return str
-    else
-        local diff = len - strlen
-        if diff > 1 then
-            return (" "):rep(math.floor(len / 2)) .. str .. (" "):rep(math.floor(len / 2))
-        else
-            return str .. " "
-        end
-    end
-end
+local delim = " %#SlDelim#| "
 -- }}}
 
 -- Mode Element {{{
 -- Change color with mode
 local mode_to_hl_group = {
-    ---@format disable
-    n      = "Normal",
-    i      = "Insert",
-    c      = "Command",
-    v      = "Visual",
-    V      = "Visual",
-    [""] = "Visual",
-    [""] = "Visual",
-    ["S"]  = "Visual",
-    s      = "Visual",
-    t      = "Insert",
-    R      = "Replace",
-    r      = "Command",
-    ["!"]  = "Command",
-    ---@format enabled
+    n        = "Normal",
+    i        = "Insert",
+    c        = "Command",
+    v        = "Visual",
+    V        = "Visual",
+    ["\x16"] = "Visual",
+    ["\x13"] = "Visual",
+    ["S"]    = "Visual",
+    s        = "Visual",
+    t        = "Insert",
+    R        = "Replace",
+    r        = "Command",
+    ["!"]    = "Command",
 }
 
 local mode_to_name = {
-    ---@format disable
-    ["n"]    = "n",
-    ["no"]   = "o",
-    ["nov"]  = "_o",
-    ["noV"]  = "O",
-    ["no"] = "^O",
+    ["n"]      = "N",
+    ["no"]     = "o",
+    ["nov"]    = "_o",
+    ["noV"]    = "O",
+    ["no\x16"] = "^O",
 
-    ["v"]    = "v",
-    ["V"]    = "V",
-    [""]   = "^V",
-    [""]   = "^S",
+    ["v"]      = "v",
+    ["V"]      = "V",
+    ["\x16"]   = "^V",
+    ["\x13"]   = "^S",
 
-    ["R"]    = "r",
-    ---@format enabled
 }
 
 local function update_mode()
@@ -72,11 +55,9 @@ local function update_mode()
     local short = mode:sub(1, 1)
     local hl = mode_to_hl_group[short]
 
-    return string.format("%%#SlSMode%s#%%#SlMode%s#%s%%#SlSMode%s#",
+    return string.format("%%#SlASL#%%#SlMode%s#%-2s%%#SlTyped#",
         hl,
-        hl,
-        padd(((has_multicursor and "C-" or "") .. (mode_to_name[mode] or short)), 3),
-        hl
+        (has_multicursor and "C-" or "") .. (mode_to_name[mode] or short)
     )
 end
 -- }}}
@@ -90,13 +71,13 @@ local function update_title()
     local changed = vim.bo[buf].modified
     local readonly = vim.bo[buf].readonly or not vim.bo[buf].modifiable
 
-    return string.format(" %%#SlASL#%%#SlA%s#%s %%#SlAText#%s %s%s%%#SlASR#",
+    return string.format("%%#SlA%s# %s %%#SlAText#%s%s%s",
         btypehighlights[kind],
         btypesymbols[kind],
         (name or "[-]"),
         (readonly and show_modified and "%#SlAReadonly#[ro]" or ""),
         (show_modified and not readonly
-            and (changed and "%#SlAChanged#~" or (" "))
+            and (changed and "%#SlAChanged##" or (" "))
             or "")
     )
 end
@@ -129,7 +110,7 @@ local function update_diagnostics()
         table.insert(res, string.format("%%#SlInfo#.%d", info))
     end
 
-    return " %#SlASL#" .. table.concat(res, " ") .. "%#SlASR#"
+    return delim .. table.concat(res, " ")
 end
 -- }}}
 
@@ -141,10 +122,10 @@ local function update_macro(ev)
             return ""
         end
 
-        return ' %#SlRegister#@' .. last
+        return (" @%s "):format(last)
     else
         local reg = vim.fn.reg_recording()
-        return ' %#SlRegister#"' .. reg .. " <-"
+        return ('->@%s '):format(reg)
     end
 end
 -- }}}
@@ -178,7 +159,7 @@ local function get_fugitive_info()
         table.insert(res, string.format("%%#SlGitHead#%s", head))
     end
 
-    return " %#SlASL#" .. table.concat(res, " ") .. "%#SlASR#"
+    return table.concat(res, " ")
 end
 
 local function update_git()
@@ -192,24 +173,24 @@ local function update_git()
     local status = vim.b[0].gitsigns_status_dict
 
     local res = {}
-    if status.added and status.added > 0 then
-        table.insert(res, string.format("%%#SlDiffAdded#%d+", status.added))
-    end
-    if status.changed and status.changed > 0 then
-        table.insert(res, string.format("%%#SlDiffChanged#%d~", status.changed))
-    end
-    if status.removed and status.removed > 0 then
-        table.insert(res, string.format("%%#SlDiffRemoved#%d-", status.removed))
-    end
     if status.head then
         table.insert(res, string.format("%%#SlGitHead#%s", status.head))
+    end
+    if status.added and status.added > 0 then
+        table.insert(res, string.format("%%#SlDiffAdded#+%d", status.added))
+    end
+    if status.changed and status.changed > 0 then
+        table.insert(res, string.format("%%#SlDiffChanged#~%d", status.changed))
+    end
+    if status.removed and status.removed > 0 then
+        table.insert(res, string.format("%%#SlDiffRemoved#-%d", status.removed))
     end
 
     if #res == 0 then
         return ""
     end
 
-    return " %#SlASL#" .. table.concat(res, " ") .. "%#SlASR#"
+    return delim .. table.concat(res, " ")
 end
 -- }}}
 
@@ -257,7 +238,7 @@ local function update_filetype()
     local conceallevel = vim.wo.conceallevel
     local conceal = conceallevel > 0 and ("conceal:%s "):format(concealcursor) or ""
 
-    return string.format("%s %s %s %s%s%s",
+    return delim .. string.format("%%#StatusLine#%s %s %s %s%s%s",
         enc,
         vim.bo.fileformat,
         indent,
@@ -284,12 +265,12 @@ end
 local sections
 local indices = {
     mode = 1,
-    title = 2,
-    git = 3,
-    diagnostics = 4,
-    macro = 5,
+    macro = 3,
+    title = 4,
+    git = 5,
+    diagnostics = 6,
 
-    words = 7,
+    words = 8,
     filetype = 9,
     lsp = 10,
 }
@@ -375,20 +356,17 @@ end)
 -- prefill the line
 -- some will be static, some only updated via autocmd, some via timer
 sections = {
-    update_mode(), -- mode
-    "",            -- title of buffer with modified etc
-    "",            -- git
-    "",            -- diagnostics
-    "",            -- macro register
+    update_mode(),                 -- mode
+    " %-5(%S%)",                   -- typed keys
+    "",                            -- macro register
+    "",                            -- title of buffer with modified etc
+    "",                            -- git
+    "",                            -- diagnostics
 
-    -- current typed command, right align, start of counts
-    " %#SlKeys#%-4(%S%)%=%#SlASL#%#SlAText#",
-    "", -- counts
-    -- end of counts, start of filetype info
-    "%#SlASR# %#SlAText#",
-    "", -- filetype
-    "", -- attached LSPs
-    -- end filetype info
+    delim .. "%<%*%P %3l:%-3c%= ", -- center: cursor position
+    "",                            -- counts
+    "",                            -- filetype
+    "",                            -- attached LSPs
     "%#SlASR# ",
 }
 
