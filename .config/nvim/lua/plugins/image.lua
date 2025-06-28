b78af601d1750bc9279edc5c067033cbb3a96b94
dmxk062 <mxk06@protonmail.com>
2025-06-28 19:18:54 +0200

    nvim: update image.lua

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,19 +1,25 @@
 ---@type LazySpec
 local M = {
     "3rd/image.nvim",
+    rocks = {
+        "magick"
+    },
     lazy = true,
-    ft = { "markdown", "typst" },
+    ft = { "markdown", "typst", "html", "neorg" },
     opts = {
-        -- dont show images when e.g. a cmp popup is above them
+        processor = "magick_rock",
         window_overlap_clear_enabled = true,
         window_overlap_clear_ft_ignore = {},
         max_width = 80,
         max_height = 6, -- don't show too many lines
         -- i dont care about using nvim as a file viewer, also makes lazy loading possible
         hijack_file_patterns = {},
-    },
-    dependencies = {
-        "vhyrro/luarocks.nvim",
+        integrations = {
+            markdown = {
+                -- otherwise quickly becomes annoying or slow
+                only_render_image_at_cursor = true,
+            }
+        }
     }
 }
 
