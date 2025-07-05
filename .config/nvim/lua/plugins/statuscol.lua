781262f796d9b949e67d4c254d90b1294c678425
dmxk062 <mxk06@protonmail.com>
2025-07-05 14:56:47 +0200

    nvim: clean up statuscol

--- .config/nvim/lua/plugins/statuscol.lua
+++ .config/nvim/lua/plugins/statuscol.lua
@@ -5,44 +5,44 @@ local M = {
 
 M.config = function()
     local builtin = require("statuscol.builtin")
+
+    local segments = {
+        {
+            text = {
+                builtin.foldfunc,
+            },
+            click = "v:lua.ScFa"
+        },
+        {
+            sign = {
+                namespace = { "gitsigns.*" },
+                maxwidth = 1,
+            },
+            click = "v:lua.ScSa",
+        },
+        {
+            sign = {
+                namespace = { ".*" },
+                auto = true
+            }
+        },
+        {
+            text = {
+                builtin.lnumfunc,
+                " "
+            },
+            condition = {
+                builtin.not_empty
+            },
+            click = "v:lua.ScLa",
+        },
+    }
     require("statuscol").setup {
         ft_ignore = {
             "help"
         },
         relculright = true,
-        segments = {
-            {
-                text = {
-                    builtin.foldfunc,
-                    ""
-                },
-                click = "v:lua.ScFa"
-            },
-            {
-                sign = {
-                    namespace = { "gitsigns.*" },
-                    maxwidth = 1,
-                },
-                click = "v:lua.ScSa",
-            },
-            {
-                text = {
-                    builtin.lnumfunc,
-                    " "
-                },
-                condition = {
-                    builtin.not_empty
-                },
-                click = "v:lua.ScLa",
-            },
-            {
-                -- for qf list
-                sign = {
-                    namespace = { "quicker.*" },
-                    auto = true,
-                }
-            }
-        },
+        segments = segments,
     }
 end
 
