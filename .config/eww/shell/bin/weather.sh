5100fddaf5e099f5c66c1fe12729b679240f8979
dmxk062 <mxk06@protonmail.com>
2025-07-05 11:32:30 +0200

    eww: switch weather source to wttr.in

--- .config/eww/shell/bin/weather.sh
+++ .config/eww/shell/bin/weather.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 CITY="Graz"
-KEY=$(< "$XDG_DATA_HOME/keys/openweather")
 
-curl -sf "http://api.openweathermap.org/data/2.5/weather?appid=${KEY}&q=${CITY}&units=metric"
-echo
+curl -sf "wttr.in/$CITY?format=j1"
