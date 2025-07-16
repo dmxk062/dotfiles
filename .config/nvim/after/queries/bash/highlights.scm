b192ad3c527cdcea637ab1e4841584cb961eadae
jhk <jhk06@mailbox.org>
2025-07-16 21:13:57 +0200

    nvim: make bash array declaration highlighting more robust

--- .config/nvim/after/queries/bash/highlights.scm
+++ .config/nvim/after/queries/bash/highlights.scm
@@ -2,21 +2,30 @@
 
 ; usually "--" as a command argument is more of a metasyntactic thing
 (command
-  argument: [ (word) @punctuation.delimiter ]
+  argument: (word) @punctuation.delimiter
   (#eq? @punctuation.delimiter "--"))
 
-; Highlight associative array declarations
-(array
-  (concatenation
-    (word) @punctuation.delimiter
-    [ (string) (number) ((word) @property) ]
-    (word) @punctuation.delimiter
-    (word) @operator
-    (#lua-match? @operator "^=")
-    (#jhk-set-length! @operator 1)
-    (#any-of? @punctuation.delimiter "]" "[")))
+; Highlight array declarations
+(declaration_command
+  (word) @_arg
+  (#any-of? @_arg "-A" "-a")
+  (variable_assignment
+    value: (array
+      (concatenation
+        (word) @punctuation.delimiter
+        [
+          (string)
+          (number)
+          (word) @property
+        ]
+        (word) @punctuation.delimiter
+        (word) @operator
+        (#lua-match? @operator "^=")
+        (#jhk-set-length! @operator 1)
+        (#any-of? @punctuation.delimiter "]" "[")))))
 
 ; make bash "keywords" behave like other languages
-((command
-   name: (command_name (word)) @keyword.return
-   (#any-of? @keyword.return "return" "exit" "break")))
+(command
+  name: (command_name
+    (word)) @keyword.return
+  (#any-of? @keyword.return "return" "exit" "break"))
