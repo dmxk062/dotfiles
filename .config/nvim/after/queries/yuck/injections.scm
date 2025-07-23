c271073839f21062a14b1fa4cd253131185c0eae
jhk <jhk06@mailbox.org>
2025-07-23 17:40:38 +0200

    nvim: inject json for yuck defvar

---
+++ .config/nvim/after/queries/yuck/injections.scm
@@ -0,0 +1,9 @@
+; extends
+
+;; the majority of defvars with a string argument are json
+(list 
+  (symbol) @_fn
+  (symbol)
+  (string (string_fragment) @injection.content)
+  (#eq? @_fn "defvar")
+  (#set! injection.language "json"))
