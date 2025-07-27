b3c399eae2fbdb8be85f0edf2e0c70c93660ca01
jhk <jhk06@mailbox.org>
2025-07-27 11:22:04 +0200

    sway: update

--- .config/sway/scripts/goto-mark.sh
+++ .config/sway/scripts/goto-mark.sh
@@ -2,7 +2,7 @@
 
 IFS=$'\t' read -ra marks < <(swaymsg -t get_marks | jq -jr '.[]|"\(.)\t"')
 
-mark="$(wayinput -t "mark" "$@" -c "${marks[@]}")"
+mark="$(wayinput -t "Mark" "$@" -c "${marks[@]}")"
 if [ -n "$mark" ]; then
     swaymsg "[con_mark=^$mark]" focus
 fi
