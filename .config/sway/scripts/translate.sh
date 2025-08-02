0355785d4adda90e10e6431ffff361b6baea1ada
jhk <jhk06@mailbox.org>
2025-08-02 17:20:29 +0200

    sway/scripts: rewrite translate in python, better UX (show language)

--- .config/sway/scripts/translate.sh
+++ .config/sway/scripts/translate.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-TEXT="$(wl-paste $1 | translate)"
-response="$(notify-send -i config-language "Translated" "$TEXT" \
+TEXT="$(wl-paste $1 | translate -p)"
+response="$(notify-send -i config-language \
+    "Translated $(echo "$TEXT" | head -n 1)" \
+    "$(echo "$TEXT" | tail -n +3)" \
     --action=copy="Copy" \
     --action=view="View")"
 case "$response" in
 copy) echo "$TEXT" | wl-copy ;;
 view)
     tmpfile="$(mktemp)"
-    echo "$TEXT" > "$tmpfile"
-    xdg-open  "$tmpfile"
+    echo "$TEXT" >"$tmpfile"
+    xdg-open "$tmpfile"
     ;;
 esac
