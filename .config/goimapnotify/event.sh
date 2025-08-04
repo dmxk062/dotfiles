78e05c28329d54b86ab70fadde469b71c115b503
jhk <jhk06@mailbox.org>
2025-08-04 21:13:46 +0200

    mail: readd notmuch, just for search though

--- .config/goimapnotify/event.sh
+++ .config/goimapnotify/event.sh
@@ -4,17 +4,20 @@
 RECENT_MESSAGE=300
 
 mbsync "$1"
-[ "$2" != "new" ] && exit
-himalaya envelope list -o json order by date desc |
-    jq -r '.[0]| (.date|strptime("%Y-%m-%d %H:%M%z")|mktime) as $time | $time, (.from.name // .from.addr), .subject, ($time | strftime("%H:%M"))' | {
-    curtime=$(date +%s)
-    read -r timestamp
-    read -r author
-    read -r subject
-    read -r date
-    if [ $((curtime - timestamp)) -gt $RECENT_MESSAGE ]; then
-        exit
-    fi
 
-    notify-send -i email "$author" "($date) $subject"
-}
+if [[ "$2" == new ]]; then
+    himalaya envelope list -o json order by date desc |
+        jq -r '.[0]| (.date|strptime("%Y-%m-%d %H:%M%z")|mktime) as $time | $time, (.from.name // .from.addr), .subject, ($time | strftime("%H:%M"))' | {
+        curtime=$(date +%s)
+        read -r timestamp
+        read -r author
+        read -r subject
+        read -r date
+        if [ $((curtime - timestamp)) -gt $RECENT_MESSAGE ]; then
+            exit
+        fi
+
+        notify-send -i email "$author" "($date) $subject"
+    }
+fi
+notmuch new &
