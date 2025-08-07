27bc652e2eac0bbada3d3915b7eb8aa23929895d
jhk <jhk06@mailbox.org>
2025-08-07 14:00:59 +0200

    nvim: update and simplify luals configuration

--- .config/nvim/lsp/luals.lua
+++ .config/nvim/lsp/luals.lua
@@ -26,38 +26,41 @@ return {
                 break
             end
         end
-        if not vim.g.is_neovim and
-            not is_in_rtp
-            and not vim.startswith(path, vim.fn.stdpath("data")) then
-            return
-        end
 
-        -- load nvim-specific libraries only for config
-        local libpaths = {
-            vim.env.VIMRUNTIME,   -- runtime files
-            "${3rd}/luv/library", -- vim.uv
-            vim.fn.stdpath("config") .. "/lua"
+        local version
+        local libs = {
+            "${3rd}/luv/library"
         }
+        if vim.g.is_neovim or is_in_rtp then
+            version = "LuaJIT"
+            -- load nvim-specific libraries only for config
+            local nvim_libs = {
+                vim.env.VIMRUNTIME, -- runtime files
+                vim.fn.stdpath("config") .. "/lua"
+            }
 
-        -- load lazy plugins for those that do use lua
-        for _, plug in pairs(require("lazy").plugins()) do
-            local dir = plug.dir .. "/lua"
-            if vim.uv.fs_stat(dir) then
-                table.insert(libpaths, dir)
+            -- load lazy plugins for those that do use lua
+            for _, plug in pairs(require("lazy").plugins()) do
+                local dir = plug.dir .. "/lua"
+                if vim.uv.fs_stat(dir) then
+                    table.insert(nvim_libs, dir)
+                end
             end
+
+            vim.list_extend(libs, nvim_libs)
         end
 
         require("config.lsp").add_setting(client, "Lua", {
             runtime = {
                 -- should hold true for any decent system
-                version = "LuaJIT",
+                version = version,
                 -- prefer plugins over specs
                 path = { "?/init.lua", "?.lua" },
                 strictPath = true
             },
             workspace = {
                 checkThirdParty = false,
-                library = libpaths,
+                library = libs,
             }
         })
     end
