5e7fd6d8873a95306b04f9a127526cdd70c0665c
jhk <jhk06@mailbox.org>
2025-08-10 08:42:17 +0200

    rofi: update

--- .config/rofi/applets/mute/run.sh
+++ .config/rofi/applets/mute/run.sh
@@ -2,5 +2,5 @@
 
 exec rofi -show mute\
     -modes "mute:$XDG_CONFIG_HOME/rofi/applets/mute/mute.sh"\
-    -theme "$XDG_CONFIG_HOME/rofi/applets/mute/theme.rasi"\
+    -theme "$XDG_CONFIG_HOME/rofi/style/theme.rasi"\
     -config "$XDG_CONFIG_HOME/rofi/applets/mute/config.rasi"
