c56a96d3d2a2b07e04b7d5e95ea19b12f68647db
jhk <jhk06@mailbox.org>
2025-08-10 15:45:46 +0200

    eww: show power draw for batteries

--- .config/eww/bin/batteries.sh
+++ .config/eww/bin/batteries.sh
@@ -6,14 +6,16 @@ BASE=/org/freedesktop/UPower
 print_devices() {
     busctl --json=short call $BUSNAME $BASE $BUSNAME EnumerateDevices | jq '.data[0][]' -r |
         while read -r dev; do
-            busctl --json=short get-property $BUSNAME "$dev" $BUSNAME.Device Percentage Model State TimeToFull TimeToEmpty PowerSupply
-        done | jq -rMcs '[ . as $a | range(0; length; 6) | (if $a[.+1].data == "" then empty else {
+            busctl --json=short get-property $BUSNAME "$dev" $BUSNAME.Device \
+            Percentage Model State TimeToFull TimeToEmpty PowerSupply EnergyRate
+        done | jq -rMcs '[ . as $a | range(0; length; 7) | (if $a[.+1].data == "" then empty else {
             name: (if $a[.+5].data == true then "Internal" else $a[.+1].data end),
             internal: $a[.+5].data,
             value: $a[.].data,
             charging: $a[.+2].data == 1,
             to_full: (if $a[.+3].data == 0 then null else $a[.+3].data end),
             to_empty: (if $a[.+4].data == 0 then null else $a[.+4].data end),
+            rate: (if $a[.+6].data == 0.0 then null else $a[.+6].data end)
         } end)]'
 }
 
